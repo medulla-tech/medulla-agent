c2b18da3abfdb5a44045dcd23bafa6f2fe5377f3
Nicolas Lécureuil <neoclust@mageia.org>
2016-08-10 17:05:30 +0200

    Remove wrong licence header

--- pulse_xmpp_agent/plugins/plugin_installplugin.py
+++ pulse_xmpp_agent/plugins/plugin_installplugin.py
@@ -1,24 +1,3 @@
-/**
- * (c) 2016 Siveo, http://http://www.siveo.net
- *
- * $Id$
- *
- * This file is part of Pulse .
- *
- * Pulse is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Pulse is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Pulse.  If not, see <http://www.gnu.org/licenses/>.
- */
- 
 # -*- coding: utf-8 -*-
 import sys, os
 
