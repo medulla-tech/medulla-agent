502c3b01f0d64590338892ff266324cafdf1e9f9
Jean-françois kneib <jean-francois.kneib@siveo.net>
2016-08-10 16:14:50 +0200

    change header file plugin

--- pulse_xmpp_agent/plugins/plugin_test.py
+++ pulse_xmpp_agent/plugins/plugin_test.py
@@ -1,25 +1,28 @@
-/**
- * (c) 2016 Siveo, http://http://www.siveo.net
- *
- * $Id$
- *
- * This file is part of Pulse .
- *
- * Pulse is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Pulse is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Pulse.  If not, see <http://www.gnu.org/licenses/>.
- */
- 
 # -*- coding: utf-8 -*-
+#
+# (c) 2015 siveo, http://www.siveo.net
+# $Id$
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2. If not, see <http://www.gnu.org/licenses/>.
+#
+#"""
+#This module is dedicated to analyse inventories sent by a Pulse 2 Client.
+#The original inventory is sent using one line per kind of
+#"""
+
 from  lib.utils import pulginprocess
 plugin={"VERSION": "2.0", "NAME" :"test"}
 @pulginprocess
