2f510e040f9d9c22411d3653d62f395687ef39b6
Jean-françois kneib <jean-francois.kneib@siveo.net>
2016-08-17 09:12:01 +0200

    select startup mode

--- pulse_xmpp_agent/launcher.py
+++ pulse_xmpp_agent/launcher.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from lib.networkinfo import networkagentinfo
+from lib.configuration import  parametreconf
+
 import os
 
-os.system('./connectionagent.py')
-os.system('./agentsiveojson3.py')
+if __name__ == '__main__':
+    tg = parametreconf()
+    print tg.agenttype
+    if tg.agenttype in ["relaisserver","relayserver"]:
+        os.system('./connectionagent.py')
+        os.system('./agentxmpp.py')
+    else:
+        os.system('./agentxmpp.py')
