1b21bcd03d9381aded15e03904373814a46aa3f0
Nicolas Lécureuil <neoclust@mageia.org>
2016-08-17 16:45:55 +0200

    Fix typo

--- pulse_xmpp_agent/lib/agent-conffile.py
+++ pulse_xmpp_agent/lib/agent-conffile.py
@@ -6,11 +6,11 @@ import sys,os,platform
 conffilename = agentconf.ini
 
 if sys.platform.startswith('linux'):
-    fileconf = os.path.join("var", "lib" ,"pulse2", "clients" ,"config", conffilename)
+    fileconf = os.path.join("/", "var", "lib" ,"pulse2", "clients" ,"config", conffilename)
 elif sys.platform.startswith('win'):
     fileconf = os.path.join(os.environ["ProgramFiles"], "Pulse", "etc", conffilename)
 elif sys.platform.startswith('darwin'):
-    fileconf = os.path.join("Library", "Application Support", "Pulse", "etc", conffilename)
+    fileconf = os.path.join("/", "Library", "Application Support", "Pulse", "etc", conffilename)
 else:
     fileconf = conffilename
 
