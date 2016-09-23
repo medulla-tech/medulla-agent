d6dc6dc153f30d7c41c7bbff0988754a51e80f9c
Jean-françois kneib <jean-francois.kneib@siveo.net>
2016-09-23 14:39:48 +0200

    fix convertion data text file format linux to format windows

--- pulse_xmpp_agent/pluginsmachine/plugin_installplugin.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_installplugin.py
@@ -2,24 +2,23 @@
 #"""
 # 
 #"""
- 
+
 import sys, os
 
-
-
-plugin={"VERSION": "1.0", "NAME" :"installplugin"}
+plugin={"VERSION": "1.0", "NAME" : "installplugin"}
 
 def action( objetxmpp, action, sessionid, data, message, dataerreur ):
     if action == 'installplugin':
         if len(data) != 0 :
             pl = sys.platform
             if pl.startswith('win'):
-                data = data.replace("\n","\r\n");
+                data['datafile'] = str(data['datafile']).replace("\n","\r\n");
             elif pl.startswith('linux'):
                 pass
             else:
                 pass
             namefile =  os.path.join(objetxmpp.config.pathplugins,data['pluginname'])
+
             try:
                 fileplugin = open(namefile, "w")
                 fileplugin.write(str(data['datafile']))
--- pulse_xmpp_agent/pluginsrelay/plugin_installplugin.py
+++ pulse_xmpp_agent/pluginsrelay/plugin_installplugin.py
@@ -2,20 +2,18 @@
 #"""
 # 
 #"""
- 
+
 import sys, os
 
 
-
-plugin={"VERSION": "1.0", "NAME" :"installplugin"}
+plugin={"VERSION": "1.0", "NAME" : "installplugin"}
 
 def action( objetxmpp, action, sessionid, data, message, dataerreur ):
-    print "*******************************************INSTALL PLUGIN"
     if action == 'installplugin':
         if len(data) != 0 :
             pl = sys.platform
             if pl.startswith('win'):
-                data = data.replace("\n","\r\n");
+                data['datafile'] = str(data['datafile']).replace("\n","\r\n");
             elif pl.startswith('linux'):
                 pass
             else:
