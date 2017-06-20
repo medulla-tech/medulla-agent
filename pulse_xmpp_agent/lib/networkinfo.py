2c0ee620a967fd89de767715273d4770fb3da692
Nicolas Lécureuil <neoclust@mageia.org>
2017-06-20 15:50:22 +0200

    Fix imports

--- pulse_xmpp_agent/lib/networkinfo.py
+++ pulse_xmpp_agent/lib/networkinfo.py
@@ -25,7 +25,7 @@ import subprocess
 import sys
 import platform
 import utils
-from lib.utils import simplecommand
+from lib.utils import simplecommand, windowsservice
 
 if sys.platform.startswith('win'):
     import wmi
