96798b0b64d59be11dcaae972ed0549d575bfcb9
Nicolas Lécureuil <neoclust@mageia.org>
2017-09-15 16:40:30 +0200

    Fix imports

--- pulse_xmpp_agent/lib/manage_process.py
+++ pulse_xmpp_agent/lib/manage_process.py
@@ -20,8 +20,8 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 
-import sys,os
-import os.path
+import sys
+import os
 import json
 from multiprocessing import Process, TimeoutError
 import traceback
