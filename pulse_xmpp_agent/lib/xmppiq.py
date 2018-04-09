caad6e1bb581e79e1ce1ddda7ccc0f311c3fea26
jfkneib <jean-francois.kneib@siveo.net>
2018-04-09 14:39:17 +0200

    file conf .local remote edition

--- pulse_xmpp_agent/lib/xmppiq.py
+++ pulse_xmpp_agent/lib/xmppiq.py
@@ -142,7 +142,7 @@ class functionsynchroxmpp:
                     else:
                         data['data'] = { "result" : "error save config file %s missing"%filename, "error" : True , 'numerror' : 130 }
             elif data['data']['action'] == 'listconfigfile':
-                listfileedit = [ x for x in os.listdir(directoryconffile()) if x.endswith(".ini")]
+                listfileedit = [ x for x in os.listdir(directoryconffile()) if (x.endswith(".ini") or x.endswith(".ini.local"))]
                 data['data'] = data['data'] = { "result" : listfileedit, "error" : False , 'numerror' : 0 }
                 return json.dumps(data)
             else:
