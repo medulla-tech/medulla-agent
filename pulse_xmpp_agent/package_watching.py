cda3d05093b985d138f42536fdc6efa9dcd79f54
spointu <spointu@gmail.com>
2018-09-12 12:22:18 +0200

    Change log file name for package machine

--- pulse_xmpp_agent/package_watching.py
+++ pulse_xmpp_agent/package_watching.py
@@ -51,7 +51,7 @@ conf ={}
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(message)s',
-                    filename='/var/log/mmc/waychingfile.log',
+                    filename='/var/log/pulse/pulse-package-watching.log',
                     filemode='a')
 
 
