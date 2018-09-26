6808d904f6357cde49e2c4bf13caa67ecf57b2aa
spointu <spointu@gmail.com>
2018-09-26 22:47:28 +0200

    Fix VNC permissions change via quick action

--- pulse_xmpp_agent/agentxmpp.py
+++ pulse_xmpp_agent/agentxmpp.py
@@ -930,7 +930,7 @@ class MUCBot(sleekxmpp.ClientXMPP):
 
         if dataobj['action'] == "vncchangepermsfrommaster":
             askpermission = 1
-            if 'askpermission' in dataobj['data'] and dataobj['data']['askpermission'] == 0:
+            if 'askpermission' in dataobj['data'] and dataobj['data']['askpermission'] == '0':
                 askpermission = 0
 
             vnc_set_permission(askpermission)
