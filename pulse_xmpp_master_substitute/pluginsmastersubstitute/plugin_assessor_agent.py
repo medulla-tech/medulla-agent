3790712e32963a1fc00f8d9e1e24ba5240e85c4b
jfk <jean-francois.kneib@siveo.net>
2020-01-13 14:17:10 +0100

    [feature substitute log] chang structure log message
    (Cherry-picked from commit 7a4b6bb26fee43793bfacce1b0f390c1324c7c7e)

--- pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_assessor_agent.py
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_assessor_agent.py
@@ -51,7 +51,7 @@ DEBUGPULSEPLUGIN = 25
 plugin = {"VERSION" : "1.0", "NAME" : "assessor_agent", "TYPE" : "substitute", "FEATURE": "assessor" }
 
 
-def action(objectxmpp, action, sessionid, data, msg, ret, dataobj={}):
+def action(objectxmpp, action, sessionid, data, msg, ret, dataobj):
     logger.debug("=====================================================")
     logger.debug("call %s from %s"%(plugin, msg['from']))
     logger.debug("=====================================================")
@@ -360,8 +360,8 @@ def Algorithm_Rule_Attribution_Agent_Relay_Server(objectxmpp,
 		# Network Rule : 10
         elif x[0] == 10:
             # Associates relay server based on network address
-            logger.debug("Analysis the 10th rule : Associate relay server based on netmask address")
-            logger.debug("Net mask address: %s" % data['xmppmask'])
+            logger.debug("Analysis the 10th rule : Associate relay server based on netmask adress")
+            logger.debug("Net mask adress: %s" % data['xmppmask'])
             result1 = XmppMasterDatabase().algorulebynetmaskaddress(data['xmppmask'],
                                                                     data['classutil'])
             if len(result1) > 0:
@@ -401,47 +401,20 @@ def Algorithm_Rule_Attribution_Agent_Relay_Server(objectxmpp,
         arsjid = XmppMasterDatabase().getRelayServerfromjid("rspulse@pulse")
         # Start relay server agent configuration
         # we order the ARS from the least used to the most used.
-        response = { 'action': 'resultconnectionconf',
+        reponse = {'action': 'resultconnectionconf',
                     'sessionid': data['sessionid'],
                     'data': z1,
                     'syncthing' : objectxmpp.announce_server,
-                    'ret': 0}
-        if len(listars) == 0:
-            logger.warning("No configuration sent to machine "\
-                "agent %s. ARS %s is found but it is stopped." % (data['information']['info']['hostname'], result[2]))
-            logger.warning("ACTION: Re-start the ARS on %s, and wait for the agent to run its reconfiguration."%(result[2]))
-
-            objectxmpp.xmpplog("No configuration sent to machine agent %s. ARS %s is found but it is stopped." % (result[2],
-                                                                                  data['information']['info']['hostname'] ),
-                            type = 'conf',
-                            sessionname =  sessionid,
-                            priority = -1,
-                            action = "xmpplog",
-                            who = data['information']['info']['hostname'],
-                            module = "Configuration | Notify | connectionagent",
-                            date = None,
-                            fromuser = objectxmpp.boundjid.bare)
-            sendErrorConnectionConf(objectxmpp, sessionid, msg)
-            return
-
-        agentsubscription = "master@pulse"
+                    'ret': 0
+                    }
         if "substitute" in data and \
             "conflist" in data["substitute"] and \
                 len(data["substitute"]["conflist"]) > 0:
-            response["substitute"] =  XmppMasterDatabase().\
-                                    substituteinfo(data["substitute"],
-                                                   z1[0][2])
+            reponse["substitute"] =  XmppMasterDatabase().substituteinfo(data["substitute"],
+                                                                         z1[0][2])
 
-            response["substitute"]["ars_chooose_for_substitute"] = z1[0][2]
-            logger.debug("substitute resend to agent : %s"%json.dumps(response["substitute"],indent=4))
-            if "subscription" in response["substitute"]:
-                agentsubscription = response["substitute"]['subscription'][0]
-                listmacadress=[]
-                for mac in data['information']['listipinfo']:
-                    listmacadress.append(mac['macaddress'])
-                XmppMasterDatabase().setuplistSubscription(listmacadress, agentsubscription)
         objectxmpp.send_message(mto=msg['from'],
-                            mbody=json.dumps(response),
+                            mbody=json.dumps(reponse),
                             mtype='chat')
         #add account for delete
         #list des comptes a suprimer
@@ -532,13 +505,13 @@ def displayData(objectxmpp, data):
             logger.info("%s" % json.dumps(data['information'], indent=4, sort_keys=True))
 
 def sendErrorConnectionConf(objectxmpp, session,  msg):
-    response = {'action': 'resultconnectionconf',
+    reponse = {'action': 'resultconnectionconf',
                'sessionid': session,
                'data': [],
                'syncthing' : "",
                'ret': 255}
     objectxmpp.send_message(mto=msg['from'],
-                        mbody=json.dumps(response),
+                        mbody=json.dumps(reponse),
                         mtype='chat')
 
 def read_conf_ascessor(objectxmpp):
@@ -607,9 +580,9 @@ def read_conf_ascessor(objectxmpp):
                     if objectxmpp.defaultrelayserverip == ip:
                         logger.error("see parameter [serverip] in file : %s " \
                                 "if Connection server parameters " \
-                                "if no relay server is available" % pathfileconf)
+                                "if no relay server is available"%pathfileconf)
                         logging.getLogger().error('parameter section "parameters" ' \
-                                                  'serverip must not be %s' % ip)
+                                                  'serverip must not be %s'%ip)
                         objectxmpp.assessor_agent_errorconf  = True
             else:
                 logger.error("see parameter [serverip] " \
