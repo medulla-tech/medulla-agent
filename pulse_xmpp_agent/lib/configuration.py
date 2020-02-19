cc3901563bdde3f55120c1f05e8937d2a174b936
jfk <jean-francois.kneib@siveo.net>
2020-02-19 14:32:55 +0100

    [add parameter agent] paraneter delta time alternative connection ars
    (cherry picked from commit f029a48cefcf5fb0d0834041d64e6d53299b5b64)

--- pulse_xmpp_agent/lib/configuration.py
+++ pulse_xmpp_agent/lib/configuration.py
@@ -499,6 +499,24 @@ class confParameter:
             self.baseurlguacamole = ""
 
         try:
+            timeal = Config.get('global', 'alternativetimedelta')
+            self.timealternatif = [int(x) for x in
+                                   re.split(r'[a-zA-Z;,\[\(\]\)\{\}\:\=\+\*\\\?\/\#\+\.\&\-\$\|\s]\s*',
+                                            timeal)
+                                   if x.strip()!=""][:2]
+            self.timealternatif.sort()
+            if len(self.timealternatif) < 2:
+                raise
+            else:
+                if self.timealternatif[0] < 2: self.timealternatif[0] = 2
+                if self.timealternatif[1] > 30 : self.timealternatif[1] = 30
+        except Exception:
+            self.timealternatif=[2,30]
+            logger.error("\n%s"%(traceback.format_exc()))
+            logger.warning('default [Global] parameter "alternativetimedelta" is %s'%self.timealternatif)
+        logger.info('[Global] Parameter "alternativetimedelta" is %s'%self.timealternatif)
+
+        try:
             self.debug = Config.get('global', 'log_level')
         except BaseException:
             self.debug = 'NOTSET'
