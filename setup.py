7da3337bdabe5dd9520f77fa4f7c228790e96147
spointu <spointu@gmail.com>
2020-04-08 11:06:09 +0200

    [packaging] Fix deb build

---
+++ setup.py
@@ -0,0 +1,46 @@
+#
+# (c) 2016-2020 siveo, http://www.siveo.net
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+
+from setuptools import setup
+
+setup(
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GPL License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    keywords='pulse-xmpp-agent',
+    name='pulse-xmpp-agent',
+    version='2.0.6',
+    debian_distro='stretch',
+    description='pulse-xmpp-agent',
+    url='https://www.siveo.net/',
+    packages=['pulse-xmpp-agent'],
+    test_suite='',
+    package_data={},
+    entry_points={},
+    extras_require={},
+    install_requires=[],
+    )
