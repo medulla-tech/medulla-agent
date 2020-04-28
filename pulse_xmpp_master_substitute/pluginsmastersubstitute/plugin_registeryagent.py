5368bef275232ee443f21cccce144f361857662f
Nicolas Lécureuil <neoclust@mageia.org>
2020-04-28 19:06:06 +0200

    [substitute] Fix number of arguments

--- pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_registeryagent.py
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_registeryagent.py
@@ -732,7 +732,7 @@ def action(xmppobject, action, sessionid, data, msg, ret, dataobj):
                                                 xmppobject.boundjid.bare)
                 return
 
-            logger.info("Machine %s registered with %s" % msg['from'])
+            logger.info("Machine %s registered" % msg['from'])
             XmppMasterDatabase().setlogxmpp("Machine %s registered" % msg['from'],
                                             "info",
                                             sessionid,
