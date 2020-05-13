b05f5491a6c7ffe47f6bbd10b7b57272517c5fa0
jfk <jean-francois.kneib@siveo.net>
2020-05-13 10:17:07 +0200

    [fixe] function search protocole

--- pulse_xmpp_agent/lib/utils.py
+++ pulse_xmpp_agent/lib/utils.py
@@ -1267,6 +1267,64 @@ class protodef:
 
         return protport
 
+def protoandport():
+    protport = {}
+    if sys.platform.startswith('win'):
+        for process in psutil.process_iter():
+            if 'tvnserver.exe' in process.name():
+                process_handler = psutil.Process(process.pid)
+                for cux in process_handler.connections():
+                    if cux.status == psutil.CONN_LISTEN:
+                        protport['vnc'] = cux.laddr.port
+            elif 'sshd.exe' in process.name():
+                process_handler = psutil.Process(process.pid)
+                for cux in process_handler.connections():
+                    if cux.status == psutil.CONN_LISTEN:
+                        protport['ssh'] = cux.laddr.port
+        for service in psutil.win_service_iter():
+            if 'TermService' in service.name():
+                service_handler = psutil.win_service_get('TermService')
+                if service_handler.status() == 'running':
+                    pid = service_handler.pid()
+                    process_handler = psutil.Process(pid)
+                    for cux in process_handler.connections():
+                        if cux.status == psutil.CONN_LISTEN:
+                            protport['rdp'] = cux.laddr.port
+
+    elif sys.platform.startswith('linux'):
+        for process in psutil.process_iter():
+            if 'Xvnc' in process.name():
+                process_handler = psutil.Process(process.pid)
+                for cux in process_handler.connections():
+                    try:
+                        ip = cux.laddr[0]
+                        port = cux.laddr[1]
+                    except Exception:
+                        ip = cux.laddr.ip
+                        port = cux.laddr.port
+                    if cux.status == psutil.CONN_LISTEN and ip == "0.0.0.0":
+                        protport['vnc'] = port
+            elif 'sshd' in process.name():
+                process_handler = psutil.Process(process.pid)
+                for cux in process_handler.connections():
+                    try:
+                        ip = cux.laddr[0]
+                        port = cux.laddr[1]
+                    except Exception:
+                        ip = cux.laddr.ip
+                        port = cux.laddr.port
+                    if cux.status == psutil.CONN_LISTEN and ip == "0.0.0.0":
+                        protport['ssh'] = port
+
+    elif sys.platform.startswith('darwin'):
+        for process in psutil.process_iter():
+            if 'ARDAgent' in process.name():
+                protport['vnc'] = '5900'
+        for cux in psutil.net_connections():
+            if cux.laddr.port == 22 and cux.status == psutil.CONN_LISTEN:
+                protport['ssh'] = '22'
+
+    return protport
 
 def ipfromdns(name_domaine_or_ip):
     """ This function converts a dns to ipv4
