4c7904723cd50a354091ed2800a14bfd018b1806
Nicolas Lécureuil <neoclust@mageia.org>
2020-07-12 12:09:41 +0200

    Fix docstring too long

--- pulse_xmpp_agent/lib/deamon.py
+++ pulse_xmpp_agent/lib/deamon.py
@@ -165,7 +165,9 @@ class Daemon(object):
 
     def run(self):
         """
-        You should override this method when you subclass Daemon. It will be called after the process has been
+        You should override this method when you subclass Daemon. 
+        
+        It will be called after the process has been
         daemonized by start() or restart().
         """
         raise NotImplementedError
