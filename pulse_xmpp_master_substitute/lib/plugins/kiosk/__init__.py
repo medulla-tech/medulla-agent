9fb7a8231519a86b3beaf8d7ede967f3e308ab1c
Nicolas Lécureuil <neoclust@mageia.org>
2020-07-22 01:59:35 +0200

    [QA] Fix import order

--- pulse_xmpp_master_substitute/lib/plugins/kiosk/__init__.py
+++ pulse_xmpp_master_substitute/lib/plugins/kiosk/__init__.py
@@ -25,7 +25,6 @@ kiosk database handler
 # SqlAlchemy
 from sqlalchemy import create_engine, MetaData, select, func, and_, desc, or_, distinct
 from sqlalchemy.orm import sessionmaker
-Session = sessionmaker()
 from sqlalchemy.exc import DBAPIError, NoSuchTableError
 from sqlalchemy import update
 from datetime import date, datetime, timedelta
@@ -49,6 +48,8 @@ except ImportError:
     from sqlalchemy.orm.base import _entity_descriptor
 
 from sqlalchemy.orm import scoped_session
+
+Session = sessionmaker()
 
 class Singleton(object):
 
