32bd8cabf57850d86d30cdd2e572ceae8944cdc9
Nicolas Lécureuil <neoclust@mageia.org>
2020-07-22 18:29:43 +0200

    Add pulseTempDir function

--- pulse_xmpp_agent/lib/agentconffile.py
+++ pulse_xmpp_agent/lib/agentconffile.py
@@ -51,6 +51,34 @@ def directoryconffile():
     else:
         return None
 
+
+def pulseTempDir():
+    """
+    This function permits to obtain the temporary folder.
+
+    Returns:
+        It returns the path of pulse temporary folder
+    """
+    if sys.platform.startswith('linux'):
+        fileconf = os.path.join(
+            "/",
+            "tmp")
+    elif sys.platform.startswith('win'):
+        fileconf = os.path.join(
+            os.environ["ProgramFiles"],
+            "Pulse",
+            "tmp")
+    elif sys.platform.startswith('darwin'):
+        fileconf = os.path.join(
+            "/opt",
+            "Pulse",
+            "tmp")
+    if os.path.isdir(fileconf):
+        return fileconf
+
+    return None
+
+
 def conffilename(agenttype):
     """
         This function define where the configuration file is located.
