b3a44f21ab6e6b2562002f70d9a2b4846a4bf5a8
SPO <stephane.pointu@siveo.net>
2020-09-04 12:26:57 +0200

    [inventory] Fix typo

--- pulse_xmpp_agent/pluginsmachine/plugin_inventory.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_inventory.py
@@ -38,7 +38,7 @@ if sys.platform.startswith('win'):
 DEBUGPULSEPLUGIN = 25
 ERRORPULSEPLUGIN = 40
 WARNINGPULSEPLUGIN = 30
-plugin = {"VERSION": "2.3", "NAME": "inventory", "TYPE": "machine"}
+plugin = {"VERSION": "2.4", "NAME": "inventory", "TYPE": "machine"}
 
 def action(xmppobject, action, sessionid, data, message, dataerreur):
     logger.debug("###################################################")
@@ -101,7 +101,7 @@ def action(xmppobject, action, sessionid, data, message, dataerreur):
                            action="xmpplog",
                            who=strjidagent,
                            module="Notify | Inventory | Error",
-                           ate=None)
+                           date=None)
         return
     if os.path.exists(inventoryfile):
         if os.path.exists("%s.back" % inventoryfile):
@@ -113,7 +113,7 @@ def action(xmppobject, action, sessionid, data, message, dataerreur):
             for nbcmd in range(1, 4):
                 logger.debug("process inventory %s timeout %s" % (nbcmd,
                                                                   timeoutfusion))
-                if namefilexml  and os.path.exists(namefilexml):
+                if namefilexml and os.path.exists(namefilexml):
                     cmd = "fusioninventory-agent --backend-collect-timeout=%s "\
                         "--additional-content=%s --local=%s" % (timeoutfusion,
                                                                 namefilexml,
