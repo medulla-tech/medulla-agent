fc4996d2545f9fd0fec815003e92be46a24ee57b
Nicolas Lécureuil <neoclust@mageia.org>
2020-09-07 12:10:57 +0200

    Update version to 2.1.2

--- packaging/python/agent_setup.py
+++ packaging/python/agent_setup.py
@@ -32,7 +32,7 @@ for scheme in INSTALL_SCHEMES.values():
 
 setup(
     name="pulse_xmpp_agent",
-    version="2.1.1",
+    version="2.1.2",
     author="SIVEO",
     author_email="support@siveo.net",
     description="XMPP Agent for pulse",
--- packaging/rpm/rhel/SPECS/pulse-xmpp-agent.spec
+++ packaging/rpm/rhel/SPECS/pulse-xmpp-agent.spec
@@ -6,7 +6,7 @@
 
 Summary:	Pulse XMPP Agent
 Name:		pulse-xmpp-agent
-Version:	2.1.1
+Version:	2.1.2
 %if ! %use_git
 Release:        1%{?dist}
 %else
