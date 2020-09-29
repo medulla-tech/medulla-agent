6d98e522b84b895d762dc8390400fb14094a6d47
jfk <jean-francois.kneib@siveo.net>
2020-09-29 15:19:03 +0200

    status event set 1 only last event machine alert

--- pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
+++ pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
@@ -5670,7 +5670,8 @@ class XmppMasterDatabase(DatabaseHelper):
                                        mon_machine_id,
                                        id_device_reg,
                                        doc,
-                                       status_event=1)
+                                       status_event=1, 
+                                       hostname=hostname)
             else:
                 # Check if there is a general rule for this device
                 objectlist_local_rule = self._rule_monitoring(hostname,
@@ -5687,7 +5688,8 @@ class XmppMasterDatabase(DatabaseHelper):
                                            mon_machine_id,
                                            id_device_reg,
                                            doc,
-                                           status_event=1)
+                                           status_event=1, 
+                                            hostname=hostname)
             logging.getLogger().debug("==================================")
             return id_device_reg
         except Exception as e:
@@ -5724,7 +5726,8 @@ class XmppMasterDatabase(DatabaseHelper):
                           id_machine,
                           id_device,
                           doc,
-                          status_event=1):
+                          status_event=1, 
+                          hostname=None):
 
         if objectlist_local_rule:
             # apply binding to find out if an alert or event is defined
@@ -5756,13 +5759,41 @@ class XmppMasterDatabase(DatabaseHelper):
                 #logging.getLogger().debug("bindingcmd %s"%bindingcmd)
                 #logging.getLogger().debug("z['type_event'] %s"%z['type_event'])
                 #logging.getLogger().debug("status_event %s"%status_event)
+                if hostname is not None:
+                    self.remise_status_event(z['id'], 
+                                             0,
+                                             hostname)
                 self.setMonitoring_event(id_machine,
                                          id_device,
                                          z['id'],
                                          bindingcmd,
                                          type_event=z['type_event'],
                                          status_event=1)
-
+    @DatabaseHelper._sessionm
+    def remise_status_event(self,
+                            session,
+                            id_rule, 
+                            status_event,
+                            hostname):
+        try:
+            sql="""UPDATE `xmppmaster`.`mon_event`
+                        JOIN
+                    xmppmaster.mon_machine ON xmppmaster.mon_machine.id = xmppmaster.mon_event.machines_id 
+                SET 
+                    `xmppmaster`.`mon_event`.`status_event` = '%s'
+                WHERE
+                        xmppmaster.mon_machine.hostname LIKE '%s'
+                    AND 
+                        xmppmaster.mon_event.id_rule = %s;""" % (status_event,
+                                                                 localhost,
+                                                                 id_rule)
+                        
+            result = session.execute(sql)
+            session.commit()
+            session.flush()
+        except Exception as e:
+            logging.getLogger().error(str(e))
+            return -1
 
 
     def __binding_application(self, datastring, bindingstring, device_type):
