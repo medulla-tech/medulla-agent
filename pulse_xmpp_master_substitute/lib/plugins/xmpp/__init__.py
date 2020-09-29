00a09037a386733d2631014f79d6dc3a17342250
jfk <jean-francois.kneib@siveo.net>
2020-09-29 15:40:22 +0200

    status event set 1 only last event machine alert

--- pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
+++ pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
@@ -5785,7 +5785,7 @@ class XmppMasterDatabase(DatabaseHelper):
                         xmppmaster.mon_machine.hostname LIKE '%s'
                     AND 
                         xmppmaster.mon_event.id_rule = %s;""" % (status_event,
-                                                                 localhost,
+                                                                 hostname,
                                                                  id_rule)
                         
             result = session.execute(sql)
