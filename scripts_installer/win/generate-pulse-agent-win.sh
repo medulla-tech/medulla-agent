a166554ffd42ad6ad3aee36e3f5c854c5509c66e
Nicolas Lécureuil <neoclust@mageia.org>
2020-10-14 10:06:17 +0200

    Remove extra empty lines

--- scripts_installer/win/generate-pulse-agent-win.sh
+++ scripts_installer/win/generate-pulse-agent-win.sh
@@ -61,14 +61,12 @@ PY_MODULES_32_FILENAMES="pypiwin32-219-cp27-none-win32.whl \
 pycurl-7.43.0-cp27-none-win32.whl \
 lxml-3.6.0-cp27-none-win32.whl \
 psutil-5.4.3-cp27-none-win32.whl \
-
 simplejson-3.16.0-cp27-cp27m-win32.whl \
 "
 PY_MODULES_64_FILENAMES="pypiwin32-219-cp27-none-win_amd64.whl \
 pycurl-7.43.0-cp27-none-win_amd64.whl \
 lxml-3.6.0-cp27-none-win_amd64.whl \
 psutil-5.4.3-cp27-none-win_amd64.whl \
-
 simplejson-3.16.0-cp27-cp27m-win_amd64.whl \
 "
 PY_MODULES_COMMON_FILENAMES="netifaces-0.10.5.tar.gz \
@@ -92,13 +90,9 @@ urllib3-1.22-py2.py3-none-any.whl \
 certifi-2019.3.9-py2.py3-none-any.whl \
 chardet-3.0.4-py2.py3-none-any.whl \
 pathlib-1.0.1.tar.gz \
-
 CherryPy-8.9.1-py2.py3-none-any.whl \
-
 Routes-2.4.1-py2.py3-none-any.whl \
-
 repoze.lru-0.7-py3-none-any.whl \
-
 WebOb-1.8.5-py2.py3-none-any.whl \
 "
 PULSE_AGENT_MODULE="pulse_xmpp_agent"
