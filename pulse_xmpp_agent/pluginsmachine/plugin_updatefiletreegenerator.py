9e6c00adb4d87d0ef30de99673f2b37ea6b67206
Nicolas Lécureuil <neoclust@mageia.org>
2020-12-01 14:16:47 +0100

    [plugin filetreegenerator] Improve debug

--- pulse_xmpp_agent/pluginsmachine/plugin_updatefiletreegenerator.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_updatefiletreegenerator.py
@@ -78,7 +78,7 @@ def updatefiletreegeneratorversion(version):
             utils.simplecommand(cmd)
 
 def updatefiletreegenerator(xmppobject, installed_version):
-    logger.info("Updating Filetree Generator to version %s" % FILETREEVERSION)
+    logger.info("Updating Filetree Generator from version %s to version %s" % (installed_version, FILETREEVERSION))
     if sys.platform.startswith('win'):
         pulsedir_path = os.path.join(os.environ["ProgramFiles"], "Pulse", "bin")
 
