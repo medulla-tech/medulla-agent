a2d73957600e1269142a3a07ddfdd83c47e26f96
Nicolas Lécureuil <neoclust@mageia.org>
2020-12-18 19:06:11 +0100

    [managedeployscheduler] Remove database files Every time with are in the exception we remove the database file. In the standart exception, in addition we add the type of exception we encounter.

--- pulse_xmpp_agent/lib/managedeployscheduler.py
+++ pulse_xmpp_agent/lib/managedeployscheduler.py
@@ -76,6 +76,8 @@ class manageschedulerdeploy:
                 self.dbcmdscheduler     = bsddb.btopen(self.name_basecmd , 'c')
             except Exception as error:
                 logger.error("Opening the bsddb database failed with the error \n %s" % error)
+                os.remove(self.name_basecmd)
+                self.dbcmdscheduler     = bsddb.btopen(self.name_basecmd , 'c')
 
             try:
                 self.dbsessionscheduler = bsddb.btopen(self.name_basesession , 'c')
@@ -85,6 +87,8 @@ class manageschedulerdeploy:
                 self.dbsessionscheduler = bsddb.btopen(self.name_basesession , 'c')
              except Exception as error:
                 logger.error("Opening the bsddb database failed with the error \n %s" % error)
+                os.remove(self.name_basecmd)
+                self.dbsessionscheduler = bsddb.btopen(self.name_basesession , 'c')
 
     def closebase(self):
         self.dbcmdscheduler.close()
