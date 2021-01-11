a4ce6182ac7f665a53e4d456efb3660f46242aee
jfkneib <jean-francois.kneib@siveo.net>
2021-01-11 16:22:29 +0100

    [plugin ars guacamoleconf] rewrite plugin guacamoleconf
    (cherry picked from commit 4ea0e90d7ea376548c0c482095ed38e475b02e51)

--- pulse_xmpp_agent/pluginsrelay/plugin_guacamoleconf.py
+++ pulse_xmpp_agent/pluginsrelay/plugin_guacamoleconf.py
@@ -19,147 +19,248 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 # MA 02110-1301, USA.
 # file : pluginsrelay/plugin_guacamoleconf.py
-import sys
-from lib import utils
+
 import MySQLdb
 import traceback
 import socket
-
+import base64
 import json
 import logging
 
-plugin = {"VERSION": "2.0", "NAME" :"guacamoleconf", "TYPE":"relayserver"}
+class GuacamoleError(Exception):
+       pass
+
+plugin = {"VERSION": "2.1", "NAME" :"guacamoleconf", "TYPE":"relayserver"}
 logger = logging.getLogger()
 
-def get_free_tcp_port():
-    tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    tcp.bind(('', 0))
-    addr, port = tcp.getsockname()
-    tcp.close()
+def get_free_tcp_port(objectxmpp):
+    port = -1
+    try:
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.bind(('', 0))
+        addr, port = tcp.getsockname()
+    except Exception as e:
+        errorstr = "%s" % traceback.format_exc()
+        logger.error("\n%s" % (errorstr))
+        errorstr = "error search free port for reverse VNC : %s\n" \
+                   "REMOTE traceback on %s\n"\
+                                        "%s" % (str(e),
+                                                objectxmpp.boundjid.bare,
+                                                errorstr)
+    finally:
+        tcp.close()
     return port
 
 def insertprotocole(protocole, hostname):
-    return """INSERT INTO guacamole_connection (connection_name, protocol) VALUES ( '%s_%s', '%s');"""%(protocole.upper(), hostname, protocole.lower())
+    logger.debug("new commection machine %s_%s protcol %s" % (protocole.upper(),
+                                                              hostname,
+                                                              protocole.lower()))
+    return """INSERT
+                INTO guacamole_connection (connection_name, protocol)
+                    VALUES ( '%s_%s', '%s');""" % (protocole.upper(),
+                                                   hostname,
+                                                   protocole.lower())
 
 def deleteprotocole(protocole, hostname):
-    return """DELETE FROM `guacamole_connection` WHERE connection_name = '%s_%s';"""%(protocole.upper(), hostname)
+    logger.debug("supp old commection for : %s_%s" % (protocole.upper(),
+                                                      hostname))
+    return """DELETE FROM `guacamole_connection`
+                     WHERE connection_name = '%s_%s';"""%(protocole.upper(),
+                                                          hostname)
 
 def insertparameter(index, parameter, value):
-    return """INSERT INTO guacamole_connection_parameter (connection_id, parameter_name, parameter_value) VALUES (%s, '%s', '%s');"""%(index, parameter, value)
+    logger.debug("new params in guacamole base : %s is %s" % (parameter,
+                                                              value))
+    return """INSERT
+                 INTO guacamole_connection_parameter (connection_id,
+                                                      parameter_name,
+                                                      parameter_value)
+                 VALUES (%s, '%s', '%s');"""%(index,
+                                              parameter,
+                                              value)
 
-@utils.pluginprocess
-def action(objetxmpp, action, sessionid, data, message, dataerreur, result):
+def action(objectxmpp, action, sessionid, data, message, dataerreur):
     logger.debug("###################################################")
     logger.debug("call %s from %s"%(plugin, message['from']))
     logger.debug("###################################################")
     logger.debug(json.dumps(data, indent=4))
     logger.debug("###################################################")
+    resultaction = "result%s" % action
+    result = {}
+    result['action'] = resultaction
+    result['ret'] = 0
+    result['sessionid'] = sessionid
+    result['base64'] = False
+    result['data'] = {}
+    dataerreur['action'] = resultaction
+    dataerreur['data']['msg'] = "ERROR : %s" % action
+    dataerreur['sessionid'] = sessionid
     try:
-        db = MySQLdb.connect(host=objetxmpp.config.guacamole_dbhost,
-                             user=objetxmpp.config.guacamole_dbuser,
-                             passwd=objetxmpp.config.guacamole_dbpasswd,
-                             db=objetxmpp.config.guacamole_dbname)
-    except Exception as e:
-        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
-        traceback.print_exc(file=sys.stdout)
-        raise
-    cursor = db.cursor()
-    result['data']['uuid'] = data['uuid']
-    result['data']['machine_id'] = data['machine_id']
-    result['data']['connection'] = {}
+        try:
+            db = MySQLdb.connect(host=objectxmpp.config.guacamole_dbhost,
+                                user=objectxmpp.config.guacamole_dbuser,
+                                passwd=objectxmpp.config.guacamole_dbpasswd,
+                                db=objectxmpp.config.guacamole_dbname)
+            logger.debug("connect with params\n" \
+                         "\thost is : %s\n" \
+                         "\tuser is : %s\n" \
+                         "\tdb   is : %s\n" %( objectxmpp.config.guacamole_dbhost,
+                                              objectxmpp.config.guacamole_dbuser,
+                                              objectxmpp.config.guacamole_dbname))
+        except Exception as e:
+            errorstr = "%s" % traceback.format_exc()
+            logger.error("\n%s" % (errorstr))
+            dataerreur['data']['msg'] = "REMOTE MySQL Error: %s on %s\n" \
+                                        "traceback\n"\
+                                        "%s" % (str(e),
+                                                objectxmpp.boundjid.bare,
+                                                errorstr)
+            raise GuacamoleError("connect error")
 
-    # Add only detected protocols
-    if hasattr(objetxmpp.config, 'guacamole_protocols'):
-        protos = list(set(objetxmpp.config.guacamole_protocols.split()) \
-                      & set(data['remoteservice'].keys()))
-    else:
-        protos = data['remoteservice'].keys()
 
-    try:
-        # delete connection
-        for proto in protos:
-            cursor.execute(deleteprotocole(proto, data['hostname']))
-            db.commit()
-        # create connection
-        for proto in protos:
-            result['data']['connection'][proto.upper()] = -1
-            cursor.execute(insertprotocole(proto, data['hostname']))
-            db.commit()
-            result['data']['connection'][proto.upper()] = cursor.lastrowid
-    except MySQLdb.Error, e:
-        db.close()
-        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
-        traceback.print_exc(file=sys.stdout)
-        raise
-    except Exception, e:
-        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
-        traceback.print_exc(file=sys.stdout)
-        db.close()
-        raise
-    ###################################
-    ## configure parameters
-    ###################################
-    try:
-        for proto in protos:
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            sock.settimeout(5.0)
-            try:
-                sock.connect((data['machine_ip'], int(data['remoteservice'][proto])))
-                # Machine is directly reachable. We will not need a reversessh connection
-                hostname = data['machine_ip']
-                cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'hostname', hostname))
-                port = data['remoteservice'][proto]
-                cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'port', port))
-            except socket.error:
-                # Machine is not reachable. We will need a reversessh connection
-                hostname = 'localhost'
-                cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'hostname', hostname))
-                port = get_free_tcp_port()
-                cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'port', port))
-                if proto.upper() == 'VNC':
-                    # We need additional options for reverse VNC
-                    listen_timeout = 50000
-                    cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'listen-timeout', listen_timeout))
-                    reverse_connect = 'true'
-                    cursor.execute(insertparameter(result['data']['connection'][proto.upper()], 'reverse-connect', reverse_connect))
-            sock.close()
+        cursor = db.cursor()
+        result['data']['uuid'] = data['uuid']
+        result['data']['machine_id'] = data['machine_id']
+        result['data']['connection'] = {}
 
-            # Options specific to a protocol
-            for option in objetxmpp.config.__dict__.keys():
-                if option.startswith(proto.lower()):
-                    if option == 'ssh_keyfile':
-                        # specific processing for ssh key
-                        with open(objetxmpp.config.ssh_keyfile, 'r') as keyfile:
-                            keydata=keyfile.read()
+        # Add only detected protocols
+        if hasattr(objectxmpp.config, 'guacamole_protocols'):
+            protos = list(set(objectxmpp.config.guacamole_protocols.split()) \
+                        & set(data['remoteservice'].keys()))
+        else:
+            protos = data['remoteservice'].keys()
 
-                        cursor.execute(insertparameter(\
-                            result['data']['connection'][proto.upper()],
-                           'private-key', keydata))
+        try:
+            # delete connection
+            for proto in protos:
+                cursor.execute(deleteprotocole(proto,
+                                               data['hostname']))
+                db.commit()
+            # create connection
+            for proto in protos:
+                result['data']['connection'][proto.upper()] = -1
+                cursor.execute(insertprotocole(proto, data['hostname']))
+                db.commit()
+                result['data']['connection'][proto.upper()] = cursor.lastrowid
+        except MySQLdb.Error as e:
+            errorstr = "%s" % traceback.format_exc()
+            logger.error("\n%s" % (errorstr))
+            dataerreur['data']['msg'] = "REMOTE MySQL Error: %s on %s\n" \
+                                        "traceback\n"\
+                                        "%s" % (str(e),
+                                                objectxmpp.boundjid.bare,
+                                                errorstr)
+            raise GuacamoleError("Mysql error delete proto existant")
+        except Exception as e:
+            errorstr = "%s" % traceback.format_exc()
+            logger.error("\n%s" % (errorstr))
+            dataerreur['data']['msg'] = "REMOTE Error: %s on %s\n" \
+                                        "traceback\n"\
+                                        "%s" % (str(e),
+                                                objectxmpp.boundjid.bare,
+                                                errorstr)
+            raise GuacamoleError("error delete proto existant")
+        ###################################
+        ## configure parameters
+        ###################################
+        try:
+            for proto in protos:
+                try:
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    sock.settimeout(5.0)
+                    sock.connect((data['machine_ip'], int(data['remoteservice'][proto])))
+                    # Machine is directly reachable. We will not need a reversessh connection
+                    hostname = data['machine_ip']
+                    cursor.execute(insertparameter(result['data']['connection'][proto.upper()],
+                                                   'hostname',
+                                                   hostname))
+                    port = data['remoteservice'][proto]
+                    cursor.execute(insertparameter(result['data']['connection'][proto.upper()],
+                                                   'port',
+                                                   port))
+                except socket.error:
+                    # Machine is not reachable. We will need a reversessh connection
+                    hostname = 'localhost'
+                    cursor.execute(insertparameter(result['data']['connection'][proto.upper()],
+                                                   'hostname',
+                                                   hostname))
+                    port = get_free_tcp_port(objectxmpp)
+                    if port != -1:
+                        cursor.execute(insertparameter(result['data']['connection'][proto.upper()],
+                                                       'port',
+                                                       port))
+                        if proto.upper() == 'VNC':
+                            # We need additional options for reverse VNC
+                            listen_timeout = 50000
+                            cursor.execute(insertparameter(result['data']['connection'][proto.upper()],
+                                                           'listen-timeout',
+                                                           listen_timeout))
+                            reverse_connect = 'true'
+                            cursor.execute(insertparameter(result['data']['connection'][proto.upper()],
+                                                           'reverse-connect',
+                                                           reverse_connect))
                     else:
-                        # Update account for the os
-                        if option[4:] == "username":
-                            username = "pulseuser"
+                        logger.error("search free reverse VNC error")
+                finally:
+                    sock.close()
+
+                # Options specific to a protocol
+                for option in objectxmpp.config.__dict__.keys():
+                    if option.startswith(proto.lower()):
+                        if option == 'ssh_keyfile':
+                            # specific processing for ssh key
+                            with open(objectxmpp.config.ssh_keyfile, 'r') as keyfile:
+                                keydata=keyfile.read()
 
                             cursor.execute(insertparameter(\
-                               result['data']['connection'][proto.upper()],
-                               "username", username))
+                                result['data']['connection'][proto.upper()],
+                            'private-key', keydata))
                         else:
+                            # Update account for the os
+                            if option[4:] == "username":
+                                username = "pulseuser"
 
-                            cursor.execute(insertparameter(\
-                               result['data']['connection'][proto.upper()],
-                               option[4:],
-                               getattr(objetxmpp.config, option)))
-            # Commit our queries
-            db.commit()
-
-    except MySQLdb.Error, e:
+                                cursor.execute(insertparameter(\
+                                                result['data']['connection'][proto.upper()],
+                                                "username",
+                                                username))
+                            else:
+                                cursor.execute(insertparameter(\
+                                                result['data']['connection'][proto.upper()],
+                                                option[4:],
+                                                getattr(objectxmpp.config,
+                                                        option)))
+                    # Commit our queries
+                    db.commit()
+        except MySQLdb.Error as e:
+            errorstr = "%s" % traceback.format_exc()
+            logger.error("\n%s" % (errorstr))
+            dataerreur['data']['msg'] = "REMOTE MySQL Error: %s on %s\n" \
+                                        "traceback\n"\
+                                        "%s" % (str(e),
+                                                objectxmpp.boundjid.bare,
+                                                errorstr)
+            raise GuacamoleError("Mysql error insert proto existant")
+        except Exception as e:
+            errorstr = "%s" % traceback.format_exc()
+            logger.error("\n%s" % (errorstr))
+            dataerreur['data']['msg'] = "REMOTE Error: %s on %s\n" \
+                                        "traceback\n"\
+                                        "%s" % (str(e),
+                                                objectxmpp.boundjid.bare,
+                                                errorstr)
+            raise GuacamoleError("error insert proto existant")
+    except Exception as e:
+        logger.error("Guacamole conf error %s" % (str(e)))
+        objectxmpp.send_message(mto=message['from'],
+                               mbody=json.dumps(dataerreur),
+                               mtype='chat')
+    finally:
         db.close()
-        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
-        traceback.print_exc(file=sys.stdout)
-        raise
-    except Exception, e:
-        dataerreur['data']['msg'] = "MySQL Error: %s" % str(e)
-        traceback.print_exc(file=sys.stdout)
-        db.close()
-        raise
-    db.close()
+        # send message result conf guacamol.
+        if result['base64'] is True:
+            result['data'] = base64.b64encode(json.dumps(result['data']))
+        logger.debug("Send message %s" % result)
+        objectxmpp.send_message(mto=message['from'],
+                                mbody=json.dumps(result),
+                                mtype='chat')
