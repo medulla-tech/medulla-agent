f9baf6f157632e5052974b3fbfa8a60c39215c9f
Nicolas Lécureuil <neoclust@mageia.org>
2021-04-23 12:29:57 +0200

    (registeryagent plugin) Change some debug to be shown if showinfobool is enabled

--- pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_registeryagent.py
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_registeryagent.py
@@ -969,7 +969,8 @@ def test_consolidation_inventory(xmppobject, sessionid, data, showinfobool, msg,
                                                                                                     idmachine))
                 setupuuid = getMachineInformationByUuidMachine(uuid, showinfobool=True)
                 if setupuuid:
-                    logger.debug("setupuuid %s" % setupuuid)
+                    if showinfobool:
+                        logger.info("setupuuid %s" % setupuuid)
                     # structure machine de glpi_computer table pour uuid setup .data['uuid_serial_machine']
                     # on a 1 setup uuid on consolide xmpp et glpi sur uuid_serial_machine
                     uuid = 'UUID' + str(setupuuid['data']['id'][0])
