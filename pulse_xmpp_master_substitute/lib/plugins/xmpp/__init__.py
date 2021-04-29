016182945bc801372829d7aa61f3f50efa86e6bb
jfkneib <jean-francois.kneib@siveo.net>
2021-04-29 10:08:41 +0200

    (sqlalchemy) use .first() function one_or_none sqlalchemy has been introduced in SQLAlchey1.0.9 which is not centos 7

--- pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
+++ pulse_xmpp_master_substitute/lib/plugins/xmpp/__init__.py
@@ -210,11 +210,11 @@ class XmppMasterDatabase(DatabaseHelper):
                                name):
         """
             this functions addition ou update table in table log xmpp.
-        #"""
+        """
         try:
             q = session.query(Agentsubscription)
             q = q.filter(Agentsubscription.name==name)
-            record = q.one_or_none()
+            record = q.first()
             if record:
                 record.name = name
                 session.commit()
@@ -224,6 +224,7 @@ class XmppMasterDatabase(DatabaseHelper):
                 return self.setagentsubscription(name)
         except Exception, e:
             logging.getLogger().error(str(e))
+            return None
 
     @DatabaseHelper._sessionm
     def setSubscription(self,
@@ -256,7 +257,7 @@ class XmppMasterDatabase(DatabaseHelper):
         try:
             q = session.query(Subscription)
             q = q.filter(Subscription.macadress==macadress)
-            record = q.one_or_none()
+            record = q.first()
             if record:
                 record.macadress = macadress
                 record.idagentsubscription = idagentsubscription
@@ -267,6 +268,7 @@ class XmppMasterDatabase(DatabaseHelper):
                 return self.setSubscription(macadress, idagentsubscription)
         except Exception, e:
             logging.getLogger().error(str(e))
+            return None
 
     @DatabaseHelper._sessionm
     def setuplistSubscription(self,
