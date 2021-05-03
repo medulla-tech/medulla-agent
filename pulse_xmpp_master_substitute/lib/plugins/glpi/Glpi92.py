1123bce33b6607a6dd120f046f3ab1adc151a9c2
jfkneib <jean-francois.kneib@siveo.net>
2021-05-03 12:12:51 +0200

    (glpi) Fix rendering of accentuated letters from glpi to pulse.

--- pulse_xmpp_master_substitute/lib/plugins/glpi/Glpi92.py
+++ pulse_xmpp_master_substitute/lib/plugins/glpi/Glpi92.py
@@ -3782,7 +3782,7 @@ class Glpi92(DatabaseHelper):
         session.close()
         return ret
 
-    def _machineobjectdymresult(self, ret):
+    def _machineobjectdymresult(self, ret, encode= 'iso-8859-1'):
         """
             this function return dict result sqlalchimy
         """
@@ -3808,10 +3808,14 @@ class Glpi92(DatabaseHelper):
                                 if isinstance(getattr(ret, keynameresult), datetime.datetime):
                                     resultrecord[keynameresult] = getattr(ret, keynameresult).strftime("%m/%d/%Y %H:%M:%S")
                                 else:
-                                    if isinstance(getattr(ret, keynameresult), basestring):
-                                        resultrecord[keynameresult] =  getattr(ret, keynameresult).decode('utf-8',  errors='ignore')
+                                    strre = getattr(ret, keynameresult)
+                                    if isinstance(strre, basestring):
+                                        if encode != "utf8":
+                                            resultrecord[keynameresult] =  "%s"%strre.decode(encode).encode('utf8')
+                                        else:
+                                            resultrecord[keynameresult] =  "%s"%strre.encode('utf8')
                                     else:
-                                        resultrecord[keynameresult] = getattr(ret, keynameresult)
+                                        resultrecord[keynameresult] = strre
                     except AttributeError:
                         resultrecord[keynameresult] = ""
         except Exception as e:
