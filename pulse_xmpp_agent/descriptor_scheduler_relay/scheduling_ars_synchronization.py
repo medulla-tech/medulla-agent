8a721d4f17d95bc4b6f45847c04c51f2e52d87ed
jfkneib <jean-francois.kneib@siveo.net>
2021-08-17 14:28:02 +0200

    (scheduling_ars_synchronization) Fix defining how many time the plugin have been called.

--- pulse_xmpp_agent/descriptor_scheduler_relay/scheduling_ars_synchronization.py
+++ pulse_xmpp_agent/descriptor_scheduler_relay/scheduling_ars_synchronization.py
@@ -37,10 +37,10 @@ def schedule_main(xmppobject):
     logger.info("=================================================\n")
     if xmppobject.config.agenttype in ['relayserver']:
         try:
-            pkgsdata = pkgsbase(xmppobject)
-
             if xmppobject.num_call_scheduling_ars_synchronization == 0:
                 __read_conf_scheduling_ars_synchronization(xmppobject, pkgsdata)
+            pkgsdata = pkgsbase(xmppobject)
+
             lien_obj = lncreate(xmppobject.config.list_folder_sources,
                                 xmppobject.config.location_packages)
             # Only for ARS
