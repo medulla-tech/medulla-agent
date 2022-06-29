ebedf3e8d32a1ad6d131c30761ce74d950067122
jfkneib <jean-francois.kneib@siveo.net>
2022-06-29 08:54:00 +0200

    registration new jid

--- pulse_xmpp_agent/agentxmpp.py
+++ pulse_xmpp_agent/agentxmpp.py
@@ -90,8 +90,6 @@ from lib.utils import (
     serialnumbermachine,
     file_put_contents_w_a,
     os_version,
-    base_message_queue_posix,
-    file_message_iq,
 )
 from lib.manage_xmppbrowsing import xmppbrowsing
 from lib.manage_event import manage_event
@@ -250,7 +248,6 @@ class MUCBot(slixmpp.ClientXMPP):
         logging.log(
             DEBUGPULSE, "start machine  %s Type %s" % (conf.jidagent, conf.agenttype)
         )
-        self.iq_msg = file_message_iq(dev_mod=True)
         self.pidprogrammprincipal = pidprogrammprincipal
 
         # create mutex
@@ -743,7 +740,6 @@ class MUCBot(slixmpp.ClientXMPP):
             )
         )
 
-        base_message_queue_posix().clean_file_all_message(prefixe=self.boundjid.user)
 
         if self.config.agenttype in ["relayserver"]:
             from lib.manage_info_command import manage_infoconsole
@@ -1200,8 +1196,48 @@ class MUCBot(slixmpp.ClientXMPP):
             logging.error("No response from server.")
             self.disconnect(wait=10)
 
+    async def register(self, iq):
+        """
+        Fill out and submit a registration form.
+
+        The form may be composed of basic registration fields, a data form,
+        an out-of-band link, or any combination thereof. Data forms and OOB
+        links can be checked for as so:
+
+        if iq.match('iq/register/form'):
+            # do stuff with data form
+            # iq['register']['form']['fields']
+        if iq.match('iq/register/oob'):
+            # do stuff with OOB URL
+            # iq['register']['oob']['url']
+
+        To get the list of basic registration fields, you can use:
+            iq['register']['fields']
+        """
+        try:
+            self.dede
+            return
+        except:
+            self.dede=1
+
+        resp = self.Iq()
+        resp['type'] = 'set'
+        resp['register']['username'] = self.boundjid.user
+        resp['register']['password'] = self.password
+
+        try:
+            await resp.send()
+            logging.info("Account created for %s!" % self.boundjid)
+        except IqError as e:
+            logging.error("Could not register account: %s" %
+                    e.iq['error']['text'])
+            self.disconnect()
+        except IqTimeout:
+            logging.error("No response from server.")
+            self.disconnect()
+
     # -----------------------------------------------------------------------
-    # --------------------- END Getion connection agent ---------------------
+    # --------------------- END Gestion connection agent ---------------------
     # -----------------------------------------------------------------------
 
     def QDeployfile(self):
@@ -2114,10 +2150,6 @@ class MUCBot(slixmpp.ClientXMPP):
                 return asyncio.get_event_loop()
 
     async def start(self, event):
-        mg = base_message_queue_posix()
-        mg.load_file(self.boundjid.user)
-        mg.clean_file_all_message(prefixe=self.boundjid.user)
-
         self.datas_send = []
         await self.get_roster()
         self.send_presence()
