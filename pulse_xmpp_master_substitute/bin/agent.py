a8669ac1e23ed1480423db48bff02d5f5933dd6d
Nicolas Lécureuil <neoclust@mageia.org>
2022-08-10 10:04:43 +0200

    (substitute agent) Do not use connect_loop_wait variable but get_connect_loop_wait instead

--- pulse_xmpp_master_substitute/bin/agent.py
+++ pulse_xmpp_master_substitute/bin/agent.py
@@ -237,7 +237,10 @@ class MUCBot(slixmpp.ClientXMPP):
         self.Mode_Marche_Arret_stop_agent(time_stop=1)
 
     def handle_disconnected(self, data):
-        self.connect_loop_wait = 2
+        logger.debug(
+            "We got disconnected. We will reconnect in %s seconds"
+            % self.get_connect_loop_wait()
+        )
 
     def register(self, iq):
         logging.info("register user %s" % self.boundjid)
