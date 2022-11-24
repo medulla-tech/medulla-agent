bf1692cce26acae88d2e6c800946a9b5015c53ea
SPO <stephane.pointu@siveo.net>
2022-11-24 08:03:58 +0100

    [user notification] Improve update of paexec

--- pulse_xmpp_agent/pluginsmachine/plugin_updatepaexec.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_updatepaexec.py
@@ -26,59 +26,96 @@ from distutils.version import StrictVersion
 import logging
 import shutil
 from lib import utils
-PAEXECVERSION = '1.29'
+import hashlib
+
+APPVERSION = '1.29'
+SHA1SUM = '0FC135B131D0BB47C9A0AAF02490701303B76D3B'
+APPNAME = 'PAExec'
+REGKEY = 'hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\%s' % APPNAME
 
 logger = logging.getLogger()
 
-plugin = {"VERSION": "1.1", "NAME": "updatepaexec", "TYPE": "machine"}
+plugin = {"VERSION": "1.5", "NAME": "updatepaexec", "TYPE": "machine"}
 
 
 def action(xmppobject, action, sessionid, data, message, dataerreur):
     logger.debug("###################################################")
     logger.debug("call %s from %s" % (plugin, message['from']))
     logger.debug("###################################################")
+
     try:
+        check_if_binary_ok()
         # Update if version is lower
-        installed_version = checkpaexecversion()
-        if StrictVersion(installed_version) < StrictVersion(PAEXECVERSION):
-            updatepaexec(xmppobject, installed_version)
+        installed_version = checkversion()
+        if StrictVersion(installed_version) < StrictVersion(APPVERSION):
+            updateapp(xmppobject, installed_version)
     except Exception:
         pass
 
 
-def checkpaexecversion():
+def check_if_binary_ok():
     if sys.platform.startswith('win'):
-        cmd = 'reg query "hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\PAExec" /s | Find "DisplayVersion"'
+        regedit = False
+        binary = False
+        reinstall = False
+
+        # We check if we have the Regedit entry
+        cmd_reg = 'reg query "%s" /s | Find "DisplayVersion"' % REGKEY
+        result_reg = utils.simplecommand(cmd_reg)
+        if result_reg['code'] == 0:
+            regedit = True
+
+        # We check if the binary is available
+        pulsedir_path = os.path.join(os.environ["ProgramFiles"], "Pulse", "bin")
+        filename = 'paexec.exe'
+
+        if os.path.isfile(os.path.join(pulsedir_path, filename)): 
+            sha1_hash = hashlib.sha1()
+            with open(os.path.join(pulsedir_path, filename),"rb") as f:
+                for byte_block in iter(lambda: f.read(4096),b""):
+                    sha1_hash.update(byte_block)
+            if sha1_hash.hexdigest().upper() == SHA1SUM:
+                binary = True
+
+        if regedit is False or binary is False: 
+            reinstall = True
+
+        if reinstall:
+            cmd = 'REG ADD "%s" /v "DisplayVersion" /t REG_SZ  /d "0.0" /f' % REGKEY
+            result = utils.simplecommand(cmd)
+            if result['code'] == 0:
+                logger.debug("%s is ready to be reinstalled." % APPNAME)
+            else:
+                logger.debug("We failed to reinitialize the registry entry.")
+
+def checkversion():
+    if sys.platform.startswith('win'):
+        cmd = 'reg query "%s" /s | Find "DisplayVersion"' % REGKEY
         result = utils.simplecommand(cmd)
         if result['code'] == 0:
-            paexecversion = result['result'][0].strip().split()[-1]
+            version = result['result'][0].strip().split()[-1]
         else:
-            # PaExec is not installed. We will force installation by returning
+            # Not installed. We will force installation by returning
             # version 0.0
-            paexecversion = '0.0'
-    return paexecversion
+            version = '0.0'
+    return version
 
-def updatepaexecversion(version):
+def updateversion(version):
     if sys.platform.startswith('win'):
-        cmd = 'REG ADD "hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\PAExec" '\
-                '/v "DisplayVersion" /t REG_SZ  /d "%s" /f' % PAEXECVERSION
+        cmd = 'REG ADD "%s" /v "DisplayVersion" /t REG_SZ  /d "%s" /f' % (REGKEY, APPVERSION)
 
         result = utils.simplecommand(cmd)
         if result['code'] == 0:
-            logger.info("we successfully updated PAExec to version %s" % PAEXECVERSION)
+            logger.info("we successfully updated %s to version %s" % (APPNAME, APPVERSION))
 
         if version == "0.0":
-            cmdDisplay = 'REG ADD "hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\\PAExec" '\
-                    '/v "DisplayName" /t REG_SZ  /d "PAExec" /f'
-	    utils.simplecommand(cmdDisplay)
-
-            cmd = 'REG ADD "hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\\PAExec" '\
-                    '/v "Publisher" /t REG_SZ  /d "SIVEO" /f'
-
+            cmdDisplay = 'REG ADD "%s" /v "DisplayName" /t REG_SZ  /d "%s" /f' % (REGKEY, APPNAME)
+            utils.simplecommand(cmdDisplay)
+            cmd = 'REG ADD "%s" /v "Publisher" /t REG_SZ  /d "SIVEO" /f' % REGKEY
             utils.simplecommand(cmd)
 
-def updatepaexec(xmppobject, installed_version):
-    logger.info("Updating PAExec from version %s to version %s" % (installed_version, PAEXECVERSION))
+def updateapp(xmppobject, installed_version):
+    logger.info("Updating %s from version %s to version %s" % (APPNAME, installed_version, APPVERSION))
     if sys.platform.startswith('win'):
         pulsedir_path = os.path.join(os.environ["ProgramFiles"], "Pulse", "bin")
 
@@ -90,7 +127,7 @@ def updatepaexec(xmppobject, installed_version):
         if result:
             # Download success
             try:
-                updatepaexecversion(installed_version)
+                updateversion(installed_version)
             except IOError as errorcopy:
                 logger.error("Error while copying the file with the error: %s" % errorcopy)
         else:
