f721e19ac6fc3979539fadd1c17f44a60e9419ff
SPO <stephane.pointu@siveo.net>
2022-12-24 04:48:28 +1300

    [deployment] Fix issue where machines are wrongly reported as offline. When a deployment is done, instead of sending a WOL, first check if machine is connected and set enabled to 1 if it is the case. (#234)
    * [deployement] ping machine off, cas substitut  subscripbe roster trop plein.
    on assure la presence dans machine.
     mais on ne met pas a jour la table uptime.
    
    (cherry picked from commit 1b1cbd84fceca1b0613af28787a65cc9502ca931)
    
    * [deployment] Fix name of plugin that manages return of ping message from the client
    
    Co-authored-by: jfkneib <jean-francois.kneib@siveo.net>

---
+++ pulse_xmpp_agent/pluginsmachine/plugin_ping.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# (c) 2016 siveo, http://www.siveo.net
+#
+# This file is part of Pulse 2, http://www.siveo.net
+#
+# Pulse 2 is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Pulse 2 is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pulse 2; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA 02110-1301, USA.
+# file : pulse_xmpp_agent/pluginsmachine/plugin_pong.py
+
+import logging
+
+plugin = {"VERSION": "1.0", "NAME": "ping",  "TYPE": "all"}
+
+logger = logging.getLogger()
+
+def action( objectxmpp, action, sessionid, data, message, dataerreur):
+    logging.getLogger().debug("###################################################")
+    logging.getLogger().debug("call %s from %s session id %s" % (plugin, message['from'], sessionid))
+    logging.getLogger().debug("###################################################")
+    datasend = {  "action" : "pong",
+                    "data" : { "agenttype" : objectxmpp.config.agenttype },
+                    'sessionid': sessionid,
+                    'ret': 0,
+                    'base64': False
+        }
+    objectxmpp.send_message(mto=message['from'],
+                                    mbody=json.dumps(datasend, indent=4),
+                                    mtype='chat')
