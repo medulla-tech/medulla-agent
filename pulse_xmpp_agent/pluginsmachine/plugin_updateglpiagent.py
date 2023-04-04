7d549722fd00f6b1e89f04262634742b934770be
Nicolas Lécureuil <neoclust@mageia.org>
2023-04-04 23:21:38 +0200

    Add GLPI Agent support.
    Now we check the agent configi info. If it exists and is glpiagent we install/use glpi agent, if not we use fusion.

--- pulse_xmpp_agent/pluginsmachine/plugin_updateglpiagent.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_updateglpiagent.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2020-2023 Siveo <support@siveo.net>
+# SPDX-FileCopyrightText: 2023 Siveo <support@siveo.net>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
 import sys
@@ -12,10 +12,10 @@ import tempfile
 import os
 
 FUSIONVERSION = '2.5.2'
-
+GLPIAGENTVERSION = '1.4'
 logger = logging.getLogger()
 
-plugin = {"VERSION": "1.3", "NAME": "updatefusion", "TYPE": "machine"}
+plugin = {"VERSION": "1.0", "NAME": "updateglpiagent", "TYPE": "machine"}
 
 
 def action(xmppobject, action, sessionid, data, message, dataerreur):
@@ -23,50 +23,45 @@ def action(xmppobject, action, sessionid, data, message, dataerreur):
     logger.debug("call %s from %s" % (plugin, message['from']))
     logger.debug("###################################################")
     try:
-        if not hasattr(xmppobject.config, 'agent') or (hasattr(xmppobject.config, 'agent') and xmppobject.config.agent != 'glpiagent'):
+        if hasattr(xmppobject.config, 'agent') and xmppobject.config.agent == 'glpiagent':
             # Update if version is lower
             check_if_binary_ok()
-            installed_version = checkfusionversion()
-            if StrictVersion(installed_version) < StrictVersion(FUSIONVERSION):
-                updatefusion(xmppobject)
-    except Exception as error_plugin:
-        logger.error("An error occured. The error code is %s" % str(e))
+            installed_version = checkGlpiAgentVersion()
+            if StrictVersion(installed_version) < StrictVersion(GLPIAGENTVERSION):
+                updateGlpiAgent(xmppobject)
+    except Exception as error:
+        logger.debug(str(error))
         pass
 
-
-def checkfusionversion():
+def checkGlpiAgentVersion():
     if sys.platform.startswith('win'):
-        cmd = 'reg query hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\FusionInventory-Agent /s | Find "DisplayVersion"'
+        cmd = 'reg query hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\{E1BE6C18-6BF4-1014-844A-F1F114E3EA24} /s | Find "DisplayVersion"'
         result = utils.simplecommand(cmd)
         if result['code'] == 0:
-            fusionversion = result['result'][0].strip().split()[-1]
-        else:
-            # Fusion is not installed. We will force installation by returning
-            # version 0.1
-            fusionversion = '0.1'
-    return fusionversion
+            glpiagentversion = result['result'][0].strip().split()[-1]
+    return glpiagentversion
 
 def check_if_binary_ok():
     if sys.platform.startswith('win'):
-        # We check if the fusion inventory binary is correctly installed.
-        fusiondir_path = os.path.join(os.environ["ProgramFiles"], "FusionInventory-Agent")
-        fusion_bin_path = os.path.join(fusiondir_path, "fusioninventory-agent.bat")
+        # We check if the GLPI-Agent inventory binary is correctly installed.
+        glpiAgentdir_path = os.path.join(os.environ["ProgramFiles"], "GLPI-Agent")
+        glpiAgent_bin_path = os.path.join(glpiAgentdir_path, "glpi-agent.bat")
 
-        if os.path.isfile(fusion_bin_path):
-            logger.debug("FusionInventory is correctly installed. Nothing to do")
+        if os.path.isfile(glpiAgent_bin_path):
+            logger.debug("Glpi-Agent is correctly installed. Nothing to do")
         else:
-            logger.error("Something went wrong while installing FusionInventory, we need to reinstall the component.")
+            logger.error("Something went wrong while installing GLPI-Agent, we need to reinstall the component.")
 
-            cmd = 'REG ADD "hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\FusionInventory-Agent" '\
+            cmd = 'REG ADD "hklm\\software\\microsoft\\windows\\currentversion\\uninstall\\{E1BE6C18-6BF4-1014-844A-F1F114E3EA24}" '\
                 '/v "DisplayVersion" /t REG_SZ  /d "0.0" /f'
             result = utils.simplecommand(cmd)
             if result['code'] == 0:
-                logger.debug("The FusionInventory module is ready to be reinstalled.")
+                logger.debug("The Glpi-Agent module is ready to be reinstalled.")
             else:
-                logger.debug("We failed to reinitialize the registry entry for FusionInventory.")
+                logger.debug("We failed to reinitialize the registry entry for Glpi-Agent.")
 
-def updatefusion(xmppobject):
-    logger.info("Updating FusionInventory Agent to version %s" % FUSIONVERSION)
+def updateGlpiAgent(xmppobject):
+    logger.info("Updating Glpi-Agent to version %s" % FUSIONVERSION)
 
     windows_tempdir = os.path.join("c:\\", "Windows", "Temp")
     install_tempdir = tempfile.mkdtemp(dir=windows_tempdir)
@@ -76,8 +71,8 @@ def updatefusion(xmppobject):
             architecture = 'x64'
         else:
             architecture = 'x86'
-        filename = 'fusioninventory-agent_windows-%s_%s.exe' % (
-            architecture, FUSIONVERSION)
+        filename = 'GLPI-Agent-%s-%s.msi' % (
+            GLPIAGENTVERSION, architecture)
         dl_url = 'http://%s/downloads/win/downloads/%s' % (
             xmppobject.config.Server, filename)
         logger.debug("Downloading %s" % dl_url)
@@ -88,7 +83,7 @@ def updatefusion(xmppobject):
             current_dir = os.getcwd()
             os.chdir(install_tempdir)
             # Run installer
-            cmd = '%s /S /acceptlicense /no-start-menu /execmode=Manual' % filename
+            cmd = 'msiexec /i %s /quiet' % filename
             cmd_result = utils.simplecommand(cmd)
             if cmd_result['code'] == 0:
                 logger.info("%s installed successfully" % filename)
