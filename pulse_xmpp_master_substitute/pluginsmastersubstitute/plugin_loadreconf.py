0d279b7728380710f359c19d5ca7062ed0136893
jfkneib <jean-francois.kneib@siveo.net>
2023-04-17 16:52:47 +0200

    [substitut reconf] On traite nbconcurrentreconf reconfiguration par cycle toutes les generate_reconf_interval si on demande la reconf d'une machine presente dans la table machine. et que cette machine n'aquicte pas sa reconf au bout de timeout_reconf alors on considere que le domaine de la machine n'est plus synchro dans la table machine. On demande 1 reconf de cette machine, dans tout les domaine xmpp.

--- pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_loadreconf.py
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_loadreconf.py
@@ -10,13 +10,13 @@ import types
 import ConfigParser
 from lib.plugins.xmpp import XmppMasterDatabase
 import time
-
+import traceback
 logger = logging.getLogger()
 DEBUGPULSEPLUGIN = 25
 
 # this plugin calling to starting agent
 
-plugin = {"VERSION": "1.0", "NAME": "loadreconf", "TYPE": "substitute"}
+plugin = {"VERSION": "1.1", "NAME": "loadreconf", "TYPE": "substitute"}
 
 def action( objectxmpp, action, sessionid, data, msg, dataerreur):
     logger.debug("=====================================================")
@@ -29,14 +29,40 @@ def action( objectxmpp, action, sessionid, data, msg, dataerreur):
         read_conf_loadreconf(objectxmpp)
         logger.debug("Configuration remote update")
         objectxmpp.concurentdata = {}
+        logger.info("install loadreconf")
         objectxmpp.loadreconf = types.MethodType(loadreconf,
                                                  objectxmpp)
+        logger.info("install send_reconf_mach_all_noeud_xmpp")
+        objectxmpp.send_reconf_mach_all_noeud_xmpp = types.MethodType(send_reconf_mach_all_noeud_xmpp,
+                                                                      objectxmpp)
+
+        logger.info("search list ars")
+        objectxmpp.list_ars =  XmppMasterDatabase().get_list_ars()
+        logger.info("%s ars used" % len(objectxmpp.list_ars))
+
         objectxmpp.listconcurentreconf = []
+
         objectxmpp.schedule('loadreconf',
                             objectxmpp.generate_reconf_interval,
                             objectxmpp.loadreconf,
                             args=(objectxmpp,),
                             repeat=True)
+
+def send_reconf_mach_all_noeud_xmpp(self, list_machine_user):
+    ## on envoi a toutes les machines de la liste dans tout les domaines
+    try:
+        datasend = {"action": "force_setup_agent",
+                    "data": "",
+                    'ret': 0,
+                    'sessionid': getRandomName(5, "loadreconf_")}
+        for ars in self.list_ars:
+            for mach in list_machine_user:
+                jidmachine="%s@%s" % ( mach, ars['domaine'])
+                self.send_message(mto=jidmachine,
+                                mbody=json.dumps(datasend),
+                                mtype='chat')
+    except Exception as e:
+        logging.getLogger().error("traceback send_reconf_mach_all_noeud_xmpp %s" % traceback.format_exc())
 
 def loadreconf(self, objectxmpp):
     """
@@ -50,41 +76,62 @@ def loadreconf(self, objectxmpp):
                 "data": "",
                 'ret': 0,
                 'sessionid': getRandomName(5, "loadreconf_")}
-    result = []
+    list_machine_enabled_reconf = []
+    viability = time.time() + objectxmpp.timeout_reconf
     while(time.time() < end):
+        # fait tant qu'on est dans le temps du cycle planifier
+        listmachine_user_search_on_ars = [x[1].split('.')[0] for x in objectxmpp.listconcurentreconf if x[2] <= t]
         listmachine_timeoutreconf = [x[0] for x in objectxmpp.listconcurentreconf if x[2] <= t]
+
         if len(listmachine_timeoutreconf) != 0:
+            # on supprime les machines qui n'ont pas ete reconfigure dans les temps et on les considere eteintes.
             logger.warning ("The following machines are currently offline and their reconfiguration will be processed later: %s" % listmachine_timeoutreconf)
+            # on lance 1 procedure de recherche pour etre certain que la machine n'a pas change de ars.
+            # on cree 1 list des users et on apelle tout les ars pour savoir si il n'ont pas cette machine conecter.
+            self.send_reconf_mach_all_noeud_xmpp(listmachine_user_search_on_ars)
+
+            # machine pas de retour de reconf on les passe a non presente.
             XmppMasterDatabase().call_set_list_machine(listmachine=listmachine_timeoutreconf)
             # on supprime les non acquites suivant timeout de plus de generate_reconf_interval seconde
             objectxmpp.listconcurentreconf = [x for x in objectxmpp.listconcurentreconf if x[2] > t]
-        viability = time.time() + objectxmpp.timeout_reconf
 
-        list_need_reconf = [ x[0] for x in objectxmpp.listconcurentreconf]
+        list_user_machine_need_reconf = [ x[1].split('.')[0] for x in objectxmpp.listconcurentreconf]
         # lists reconf terminate
-        if len(list_need_reconf) > 0:
-            resultacquite = XmppMasterDatabase().call_acknowledged_reconficuration(list_need_reconf)
+        if len(list_user_machine_need_reconf) > 0:
+            # machine
+            resultacquite = XmppMasterDatabase().call_acknowledged_reconficuration(list_user_machine_need_reconf)
+
             # liste des concurent
             if len(resultacquite) > 0:
                 logger.debug ("concurent acquite machines id %s" % resultacquite)
-            objectxmpp.listconcurentreconf = [ x for x in objectxmpp.listconcurentreconf \
-                                            if x[0] not in resultacquite]
-        if len(result) == 0:
-            result = XmppMasterDatabase().call_reconfiguration_machine(limit=objectxmpp.nbconcurrentreconf)
-            if len(result) == 0:
+                objectxmpp.listconcurentreconf = [ x for x in objectxmpp.listconcurentreconf \
+                                            if  x[1].split('.')[0]  not in resultacquite]
+
+
+
+        if len(list_machine_enabled_reconf) == 0:
+            # machine presente et reconf demande[[id,jid],...]
+            list_machine_enabled_reconf = XmppMasterDatabase().call_reconfiguration_machine(limit=objectxmpp.nbconcurrentreconf)
+            if len(list_machine_enabled_reconf) == 0:
                 return
         list_updatenopresence = []
-        while len(objectxmpp.listconcurentreconf) < objectxmpp.nbconcurrentreconf and \
-                len(result) > 0 and \
+
+
+        for _ in range(min(objectxmpp.nbconcurrentreconf, objectxmpp.listconcurentreconf)):
+            if len(list_machine_enabled_reconf) > 0 and \
                     time.time() < end:
-            eltmachine = result.pop(0)
-            eltmachine.append(viability)
-            objectxmpp.listconcurentreconf.append(eltmachine)
-            self.send_message(mto=eltmachine[1],
-                              mbody=json.dumps(datasend),
-                              mtype='chat')
-            logger.debug ("SEND RECONFIGURATION %s (%s)" % (eltmachine[1], eltmachine[0]))
-            list_updatenopresence.append(eltmachine[0])
+                eltmachine = list_machine_enabled_reconf.pop(0)
+                eltmachine.append(viability)
+                logger.debug ("ADD  %s " % (eltmachine))
+                objectxmpp.listconcurentreconf.append(eltmachine)
+                self.send_message(mto=eltmachine[1],
+                                mbody=json.dumps(datasend),
+                                mtype='chat')
+                logger.debug ("SEND RECONFIGURATION %s (%s)" % (eltmachine[1], eltmachine[0]))
+                list_updatenopresence.append(eltmachine[0])
+            else:
+                break
+
         if len(list_updatenopresence) != 0:
             XmppMasterDatabase().call_set_list_machine(listmachine=list_updatenopresence)
         time.sleep(.2)
@@ -131,6 +178,7 @@ def read_conf_loadreconf(objectxmpp):
         else:
             objectxmpp.timeout_reconf = 500
     objectxmpp.plugin_loadreconf = types.MethodType(plugin_loadreconf, objectxmpp)
+
 def plugin_loadreconf(self, msg, data):
     # Manage update remote agent
     pass
