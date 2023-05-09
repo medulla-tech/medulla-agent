fb57cf3d6cf6bc1d6b59982689d58f318602872c
tekmans <tekmans@no-log.org>
2023-05-09 10:03:25 +0200

    Upgrade fusion inventory agent up to 2.6

--- pulse_xmpp_agent/pluginsmachine/plugin_updatefusion.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_updatefusion.py
@@ -29,11 +29,11 @@ import platform
 import tempfile
 import os
 
-FUSIONVERSION = '2.5.2'
+FUSIONVERSION = '2.6'
 
 logger = logging.getLogger()
 
-plugin = {"VERSION": "1.3", "NAME": "updatefusion", "TYPE": "machine"}
+plugin = {"VERSION": "1.4", "NAME": "updatefusion", "TYPE": "machine"}
 
 
 def action(xmppobject, action, sessionid, data, message, dataerreur):
