2e433740f3ac13af1cfb2c73b146d0722d5536df
Nicolas Lécureuil <neoclust@mageia.org>
2023-07-23 00:28:51 +0200

    (installer) Fix used python version it was configured to use python 3.9 but we will use python 3.11 ( like in debian 12 )

--- scripts_installer/win/pulse-service.py
+++ scripts_installer/win/pulse-service.py
@@ -85,7 +85,7 @@ class SMWinservice(win32serviceutil.ServiceFramework):
 
 log_file = os.path.join("c:\\", "Program Files", "Pulse", "var", "log", "service.log")
 agent_dir = os.path.join(
-    "C:\\", "Program Files", "Python39", "Lib", "site-packages", "pulse_xmpp_agent"
+    "C:\\", "Program Files", "Python311", "Lib", "site-packages", "pulse_xmpp_agent"
 )
 
 logger = logging.getLogger("pulseagentservice")
@@ -161,14 +161,14 @@ class PulseAgentService(SMWinservice):
             if not re.search(filter, result):
                 if not self.isdebug:
                     os.system(
-                        os.path.join("c:\\", "Program Files", "Python39", "python.exe")
+                        os.path.join("c:\\", "Program Files", "Python311", "python.exe")
                         + " "
                         + os.path.join(agent_dir, "launcher.py")
                         + " -t machine"
                     )
                 else:
                     os.system(
-                        os.path.join("c:\\", "Program Files", "Python39", "python.exe")
+                        os.path.join("c:\\", "Program Files", "Python311", "python.exe")
                         + " "
                         + os.path.join(agent_dir, "launcher.py")
                         + " -c -t machine"
