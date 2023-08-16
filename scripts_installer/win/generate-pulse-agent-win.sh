468d5a485d28f176585d26f090d4b5ca15b2d4ab
Nicolas Lécureuil <neoclust@mageia.org>
2023-08-16 17:05:36 +0200

    (installer) Add netaddr as required module

--- scripts_installer/win/generate-pulse-agent-win.sh
+++ scripts_installer/win/generate-pulse-agent-win.sh
@@ -108,6 +108,7 @@ zc.lockfile-3.0.post1-py3-none-any.whl \
 pypiwin32-223-py3-none-any.whl \
 PyYAML-6.0.1-cp311-cp311-win_amd64.whl \
 lmdb-1.4.1-cp311-cp311-win_amd64.whl \
+netaddr-0.8.0-py2.py3-none-any.whl \
 "
 PULSE_AGENT_MODULE="pulse_xmpp_agent"
 RSYNC_DL_FILENAME="cwrsync_6.2.8_x64_free.zip"
