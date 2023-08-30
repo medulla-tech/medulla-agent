4dc1d140c47b518522f5bfa02d14f49199c4b9eb
SPO <stephane.pointu@siveo.net>
2023-08-30 17:51:35 +0200

    [py3] Workaround for scheduling_deploy plugin for issues with key, value database lib This needs a proper fix

--- pulse_xmpp_agent/descriptor_scheduler_common/scheduling_deploy.py
+++ pulse_xmpp_agent/descriptor_scheduler_common/scheduling_deploy.py
@@ -8,8 +8,9 @@ this plugin charge tous les deploy scheduler, et envoi une demand d'execution a 
 import json
 import logging
 import sys
+import traceback
 
-plugin = {"VERSION": "1.0", "NAME": "scheduling_deploy", "TYPE": "all", "SCHEDULED": True}  # fmt: skip
+plugin = {"VERSION": "1.1", "NAME": "scheduling_deploy", "TYPE": "all","SCHEDULED": True}  # fmt: skip
 
 # nb  -1 infinie
 SCHEDULE = {"schedule": "*/1 * * * *", "nb": -1}
@@ -19,31 +20,35 @@ def schedule_main(objectxmpp):
     logging.getLogger().debug("==============Plugin scheduled==============")
     logging.getLogger().debug(plugin)
     logging.getLogger().debug("============================================")
-    objectxmpp.Deploybasesched.openbase()
-    if sys.platform.startswith("darwin"):
-        for k, v in objectxmpp.Deploybasesched.dbsessionscheduler:
-            obj = json.loads(v)
-            obj["data"]["fromaction"] = obj["action"]
-            obj["action"] = "machineexecutionscheduler"
-            del obj["data"]["descriptor"]
-            del obj["data"]["packagefile"]  # ['descriptor']
-            print(json.dumps(obj, indent=4))
-            # send message to master(plugin_machineexecutionscheduler)
-            # print "SEND", json.dumps(obj, indent = 4)
-            objectxmpp.send_message(
-                mto=obj["data"]["jidmaster"], mbody=json.dumps(obj), mtype="chat"
-            )
-    else:
-        for k, v in objectxmpp.Deploybasesched.dbsessionscheduler.items():
-            obj = json.loads(v)
-            obj["data"]["fromaction"] = obj["action"]
-            obj["action"] = "machineexecutionscheduler"
-            del obj["data"]["descriptor"]
-            del obj["data"]["packagefile"]  # ['descriptor']
-            print(json.dumps(obj, indent=4))
-            # send message to master(plugin_machineexecutionscheduler)
-            # print "SEND", json.dumps(obj, indent = 4)
-            objectxmpp.send_message(
-                mto=obj["data"]["jidmaster"], mbody=json.dumps(obj), mtype="chat"
-            )
-    objectxmpp.Deploybasesched.closebase()
+    try:
+        objectxmpp.Deploybasesched.openbase()
+        if sys.platform.startswith("darwin"):
+            for k, v in objectxmpp.Deploybasesched.dbsessionscheduler:
+                obj = json.loads(v)
+                obj["data"]["fromaction"] = obj["action"]
+                obj["action"] = "machineexecutionscheduler"
+                del obj["data"]["descriptor"]
+                del obj["data"]["packagefile"]  # ['descriptor']
+                print(json.dumps(obj, indent=4))
+                # send message to master(plugin_machineexecutionscheduler)
+                # print "SEND", json.dumps(obj, indent = 4)
+                objectxmpp.send_message(
+                    mto=obj["data"]["jidmaster"], mbody=json.dumps(obj), mtype="chat"
+                )
+        else:
+            for k, v in objectxmpp.Deploybasesched.dbsessionscheduler.items():
+                obj = json.loads(v)
+                obj["data"]["fromaction"] = obj["action"]
+                obj["action"] = "machineexecutionscheduler"
+                del obj["data"]["descriptor"]
+                del obj["data"]["packagefile"]  # ['descriptor']
+                print(json.dumps(obj, indent=4))
+                # send message to master(plugin_machineexecutionscheduler)
+                # print "SEND", json.dumps(obj, indent = 4)
+                objectxmpp.send_message(
+                    mto=obj["data"]["jidmaster"], mbody=json.dumps(obj), mtype="chat"
+                )
+    except Exception:
+        logging.getLogger().error("\n%s" % (traceback.format_exc()))
+    finally :
+        objectxmpp.Deploybasesched.closebase()
