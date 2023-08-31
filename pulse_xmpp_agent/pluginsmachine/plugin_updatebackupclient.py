6858620fd9aae5943026d9fef489fbf514952026
Hruund <william.vasselin@gmail.com>
2023-08-31 10:10:57 +0200

    (URBACKUP plugin) Edit plugin for urbackup
    The urbackupclient_cmd command has change, here the new way to configure urbackup client.

--- pulse_xmpp_agent/pluginsmachine/plugin_updatebackupclient.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_updatebackupclient.py
@@ -14,7 +14,7 @@ URBACKUP_VERSION = '2.5.24'
 
 logger = logging.getLogger()
 
-plugin = {"VERSION": "1.4", "NAME": "updatebackupclient", "TYPE": "machine"}
+plugin = {"VERSION": "1.5", "NAME": "updatebackupclient", "TYPE": "machine"}
 
 
 def action(xmppobject, action, sessionid, data, message, dataerreur):
@@ -91,7 +91,7 @@ def backupclientsettings(xmppobject):
         if os.path.exists(filename):
             logger.debug("Urbackup filename: %s" % filename)
             os.chdir(urbackup_dir)
-            cmd = '"%s" set-settings -k  internet_mode_enabled -v true -k internet_server -v %s -k internet_server_port -v %s -k computername -v %s -k internet_image_backups -v true -k internet_full_file_backups -v true' % (filename, xmppobject.config.backup_server, xmppobject.config.backup_port, hostname)
+            cmd = '"%s" set-settings -k  internet_mode_enabled -v true --server-url "%s" --name "%s" --authkey "%s" -k internet_image_backups -v false -k internet_full_file_backups -v true' % (filename, xmppobject.config.backup_server, hostname, xmppobject.config.authkey)
             logger.debug("Urbackup cmd: %s" % cmd)
             cmd_result = utils.simplecommand(cmd)
             if cmd_result['code'] == 0:
