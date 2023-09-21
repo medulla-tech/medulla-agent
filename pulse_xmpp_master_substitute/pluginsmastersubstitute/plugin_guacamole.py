0e115dc430d99a1353bc82aecd39388e0eb30310
jfkneib <jean-francois.kneib@siveo.net>
2023-09-21 16:06:18 +0200

    (substitut) plugin master porte sur substitut.

---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_guacamole.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+import traceback
+import logging
+from lib.utils import name_random
+from lib.plugins.xmpp import XmppMasterDatabase
+
+logger = logging.getLogger()
+
+plugin = {"VERSION": "1.0", "NAME": "plugin_guacamole", "TYPE": "submaster"}
+# plugin run guacamole
+
+def action(xmppobject, action, sessionid, data, message, dataobj):
+    logger.debug(plugin)
+    try:
+        relayserver = XmppMasterDatabase().getRelayServerForMachineUuid(data["uuid"])
+        jidmachine = XmppMasterDatabase().getjidMachinefromuuid(data["uuid"])
+        senddataplugin = {
+            "action": "guacamole",
+            "sessionid": name_random(5, "guacamole"),
+            "data": {
+                "jidmachine": jidmachine,
+                "cux_id": int(data["cux_id"]),
+                "cux_type": data["cux_type"],
+                "uuid": data["uuid"],
+            },
+        }
+        #xmppobject.send_message(
+            #mto=relayserver["jid"],
+            #mbody=json.dumps(senddataplugin, encoding="latin1"),
+            #mtype="chat",
+        #)
+        logger.error("senddataplugin %s" % senddataplugin)
+
+        xmppobject.send_message(
+            mto=relayserver["jid"],
+            mbody=json.dumps(senddataplugin, ensure_ascii=False).encode('latin1'),
+            mtype="chat",
+        )
+
+    except:
+        logger.error("error plugin plugin_guacamole %s" % data)
+        logger.error("\n%s" % (traceback.format_exc()))
+        pass
---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_kiosk.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+import traceback
+import logging
+#from lib.utils import name_random
+#from lib.plugins.xmpp import XmppMasterDatabase
+
+logger = logging.getLogger()
+plugin = {"VERSION": "1.0", "NAME": "kiosk", "TYPE": "mastersub"}
+
+
+def action(xmppobject, action, sessionid, data, message, ret, dataobj):
+    logging.getLogger().debug("=====================================================")
+    logging.getLogger().debug(plugin)
+    logging.getLogger().debug("=====================================================")
+    print(json.dumps(data, indent=4))
+    if data["subaction"] == "send_message_to_jid":
+        if not "jid" in data:
+            logging.getLogger().error(
+                "jid missing in kiosk send_message_to_jid sub action"
+            )
+        elif not ("data" in data and "subaction" in data["data"]):
+            logging.getLogger().error("The message is not formated correctly")
+        else:
+            datasend = {
+                "action": "kiosk",
+                "sessionid": data["sessionid"],
+                "data": data["data"],
+            }
+            xmppobject.send_message(
+                mto=data["jid"], mbody=json.dumps(datasend), mtype="chat"
+            )
