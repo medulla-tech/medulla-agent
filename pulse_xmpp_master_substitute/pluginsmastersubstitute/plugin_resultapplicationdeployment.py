de0ea0f82ab2d6c5f8f5bc752b8b3d28eb2bc226
jfkneib <jean-francois.kneib@siveo.net>
2023-09-22 13:53:01 +0200

    (substitut) plugin master maintenant traite par submaster

---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_resultapplicationdeployment.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+
+logger = logging.getLogger()
+
+plugin = {"VERSION": "1.0", "NAME": "resultapplicationdeploymentjson", "TYPE": "mastersub"}
+
+
+def action(xmppobject, action, sessionid, data, message, ret, dataobj):
+    logger.debug(plugin)
+    logger.debug("%s from %s" % (data["msg"], message["from"]))
+    pass
