8b6c8672a53cf84219d7432500c657d9cb2e11e3
jfkneib <jean-francois.kneib@siveo.net>
2023-09-22 13:54:24 +0200

    (substitut) plugin master maintenant traite par submaster

---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_resultasynchroremoteQA.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; -*-
+
+import logging
+import traceback
+import sys
+import json
+from lib.plugins.xmpp import XmppMasterDatabase
+
+logger = logging.getLogger()
+
+plugin = {"VERSION": "1.0", "NAME": "resultasynchroremoteQA", "TYPE": "mastersub"}
+
+
+def action(xmppobject, action, sessionid, data, message, ret, dataobj):
+    logger.debug("=====================================================")
+    logger.debug(plugin)
+    logger.debug("=====================================================")
+    #logger.debug(json.dumps(data, indent=4))
+    try:
+        XmppMasterDatabase().setCommand_action(
+            data["data"]["data"]["uuid_inventorymachine"],
+            data["data"]["data"]["cmdid"],
+            sessionid,
+            "".join(data["result"]["result"]),
+            typemessage="result",
+        )
+
+    except Exception as e:
+        logger.error("Error loading plugin: %s" % str(e))
+        traceback.print_exc(file=sys.stdout)
+        pass
