a8363eeb46c99b66727793e6568a9f78f53806e5
jfkneib <jean-francois.kneib@siveo.net>
2023-09-22 09:08:57 +0200

    (substitut) plugin master maintenant traite par submaster

---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_applicationdeployment.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; -*-
+# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+# ce plugin est appeler pour terminer 1 session
+
+import json
+import logging
+
+
+plugin = {"VERSION": "1.0", "NAME": "applicationdeployment", "TYPE": "mastersub"}
+
+
+def action(xmppobject, action, sessionid, data, message, dataobj):
+    logging.getLogger().debug(plugin)
+    try:
+        if "Dtypequery" in data:
+            if data["Dtypequery"] == "TED":
+                print("Delete session %s" % sessionid)
+                # Set deployment to done in database
+                xmppobject.session.clear(sessionid)
+
+                if __debug__:
+                    logging.getLogger().debug(
+                        "_______________________RESULT DEPLOYMENT________________________"
+                    )
+                    logging.getLogger().debug(json.dumps(data["descriptor"]))
+                    logging.getLogger().debug(
+                        "________________________________________________________________"
+                    )
+            elif data["Dtypequery"] == "TE":
+                # clear session
+                xmppobject.session.clear(sessionid)
+                # Set deployment to error in database
+            else:
+                # Update session with data
+                xmppobject.session.sessionsetdata(sessionid, data)
+        pass
+    except Exception as e:
+        logging.getLogger().error("Error in plugin %s : %s" % (plugin["NAME"], str(e)))
