5978c32da7dc3cc27a8e354a62fe54432a87efcd
jfkneib <jean-francois.kneib@siveo.net>
2023-09-22 14:01:49 +0200

    (substitut) plugin master maintenant traite par submaster

---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_testmaster.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; -*-
+# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+from lib.utils import pluginmastersessionaction
+import logging
+# from mmc.plugins.xmppmaster.master.lib.utils import pluginmastersessionaction
+import logging
+
+plugin = {"VERSION": "1.0", "NAME": "testmaster", "TYPE": "mastersub"}
+
+
+@pluginmastersessionaction("actualise", 20)
+def action(xmppobject, action, sessionid, data, message, ret, objsessiondata):
+    logging.getLogger().debug(plugin)
+    xmppobject.session.affiche()
+    pass
