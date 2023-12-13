93919f282077eb38a7b51c4b2bd0b5d0ed7a2c62
Tekmans <tekmans@no-log.org>
2023-12-13 09:40:42 +0100

    (utils.py) Fix import convention for py3

--- pulse_xmpp_agent/lib/utils.py
+++ pulse_xmpp_agent/lib/utils.py
@@ -24,7 +24,7 @@ from pprint import pprint
 import hashlib
 import base64
 import pickle
-from agentconffile import conffilename
+from .agentconffile import conffilename
 import socket
 import psutil
 import time
