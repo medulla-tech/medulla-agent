c0e0ab271340c5fa89baae3688361577d744b12c
Nicolas Lécureuil <neoclust@mageia.org>
2024-01-21 16:19:14 +0100

    Add pydoctor file to generate the documentation

---
+++ pydoctor_config.py
@@ -0,0 +1,26 @@
+# pydoctor_config.py
+
+# Extensions de fichiers à inclure dans la documentation
+extensionsToScan = ['.py']
+
+# Liste des modules à inclure dans la documentation
+modules = ['medulla_agent']
+
+# Titre de la documentation
+projectname = 'Medulla-Agent Documentation'
+
+# Répertoire de sortie de la documentation
+docformat = 'html'
+htmloutput = 'docs'
+
+# Informations sur l'auteur
+author = 'Medulla devs'
+author_email = 'support@siveo.net'
+
+# URL du projet
+url = 'https://github.com/medulla-tech/medulla-agent'
+
+# Ajoutez d'autres options de configuration selon vos besoins.
+# Consultez la documentation officielle de pydoctor pour plus d'options : 
+# https://pydoctor.readthedocs.io/en/latest/usage/configuration.html
+
