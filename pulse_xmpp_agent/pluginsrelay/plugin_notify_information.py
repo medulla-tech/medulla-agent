f68f782e74a709d86351605d2e1bd7ca77d92109
Nicolas Lécureuil <neoclust@mageia.org>
2024-01-22 23:02:34 +0100

    Add some autogenerated documentation

--- pulse_xmpp_agent/pluginsrelay/plugin_notify_information.py
+++ pulse_xmpp_agent/pluginsrelay/plugin_notify_information.py
@@ -1,18 +1,38 @@
 # -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
-# SPDX-License-Identifier: GPL-3.0-or-later
+"""
+Notify Information Plugin
+
+SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+SPDX-License-Identifier: GPL-3.0-or-later
+
+This plugin handles actions related to notifying information.
+"""
 
 import os
 import logging
 from lib.utils import file_put_contents
 
-plugin = {"VERSION": "1.0", "VERSIONAGENT": "2.1", "NAME": "notify_information",  "TYPE": "all"}  # fmt: skip
+plugin = {"VERSION": "1.0", "VERSIONAGENT": "2.1", "NAME": "notify_information", "TYPE": "all"}  # fmt: skip
 
 logger = logging.getLogger()
 DEBUGPULSEPLUGIN = 25
 
 
 def action(objectxmpp, action, sessionid, data, message, dataerreur):
+    """
+    Perform actions based on the provided data for the notify_information plugin.
+
+    Parameters:
+    - objectxmpp: The XMPP object representing the current agent.
+    - action: The action to be performed.
+    - sessionid: The session ID associated with the action.
+    - data: The data containing information for the action.
+    - message: The XMPP message containing the request.
+    - dataerreur: Data related to any errors during the action.
+
+    Returns:
+    None
+    """
     logger.debug("###################################################")
     logger.debug("call %s from %s" % (plugin, message["from"]))
     logger.debug("sessionid : %s" % sessionid)
@@ -30,3 +50,4 @@ def action(objectxmpp, action, sessionid, data, message, dataerreur):
                 )
                 filename = os.path.join(dirtempinfo, "on_line_ars.ansible")
                 file_put_contents(filename, "boolean for ansible")
+
