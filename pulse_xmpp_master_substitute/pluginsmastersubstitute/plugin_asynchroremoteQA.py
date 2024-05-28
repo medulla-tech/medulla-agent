5a5625c2f1d510668b1e3fd147eb42934d6d9ab6
jfkneib <jean-francois.kneib@siveo.net>
2024-05-28 19:06:13 +0200

    [add plugin Quick Action] pour substitut master.
    (cherry picked from commit ec39921d86d97b05381690d3cb9ae4c6da7fcc86)
    (cherry picked from commit 08be1e87f697d6b37a7e4424d02ba68b777264a7)

---
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_asynchroremoteQA.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; -*-
+# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import logging
+import json
+
+plugin = {"VERSION": "1.0", "NAME": "asynchroremoteQA", "TYPE": "submaster"}
+
+
+def action(xmppobject, action, sessionid, data, message, dataobj):
+    logging.getLogger().debug("=====================================================")
+    logging.getLogger().debug(plugin)
+    logging.getLogger().debug("=====================================================")
+    print(json.dumps(data, indent=4))
+    datasend = {"action": action, "sessionid": sessionid, "data": data, "base64": False}
+
+    # call plugin asynchromeremoteshell to machine or relay
+    xmppobject.send_message(
+        mto=data["data"]["jid"], mbody=json.dumps(datasend), mtype="chat"
+    )
