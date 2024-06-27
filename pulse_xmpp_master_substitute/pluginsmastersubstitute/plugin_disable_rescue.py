4e5ef8d722f6d008c375dc76e6f28e1b6bb7ee27
SPO <stephane.pointu@siveo.net>
2024-06-27 11:52:09 +0200

    (quick actions) Fix quick actions by adding the missing plugins and fixing the signature of plugins (#487)
    A new signature for plugins called by master substitute replaces the obsolete signature of mmc master plugins.
    new => def action(xmppobject, action, sessionid, data, message, dataobj)
    obsolete => def action(xmppobject, action, sessionid, data, message, ret, dataobj)
    
    ---------
    
    Co-authored-by: jfkneib <jean-francois.kneib@siveo.net>

--- pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_disable_rescue.py
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_disable_rescue.py
@@ -3,22 +3,23 @@
 # SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
 # SPDX-License-Identifier: GPL-3.0-or-later
 
+"""
+This plugin can be called from quick action
+"""
 import json
 import logging
 
 logger = logging.getLogger()
 
-plugin = {"VERSION": "1.5", "NAME": "force_setup_agent", "TYPE": "mastersub"}
+plugin = {"VERSION": "1.0", "NAME": "disable_rescue", "TYPE": "master"}
 
 
 def action(xmppobject, action, sessionid, data, message, dataobj):
-    logger.debug("_________________________")
-    logger.debug(plugin)
-    logger.debug(data["data"][0])
-    logger.debug("_________________________")
-
+    logger.debug("###################################################")
+    logger.debug("call %s from %s" % (plugin, message["from"]))
+    logger.debug("###################################################")
     command = {
-        "action": "force_setup_agent",
+        "action": "disable_rescue",
         "base64": False,
         "sessionid": sessionid,
         "data": "",
