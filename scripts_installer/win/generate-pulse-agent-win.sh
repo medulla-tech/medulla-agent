1841b070b83cdeffbc254e5327a5bfdb5affdc6c
SPO <stephane.pointu@siveo.net>
2024-08-01 00:48:34 +0200

    (ansible agent) Disable geolocalisation on machines which do not have access to Internet

--- scripts_installer/win/generate-pulse-agent-win.sh
+++ scripts_installer/win/generate-pulse-agent-win.sh
@@ -204,6 +204,10 @@ check_arguments() {
                 DISABLE_INVENTORY=1
                 shift
                 ;;
+            --disable-geoloc*)
+                DISABLE_GEOLOC=1
+                shift
+                ;;
             --linux-distros*)
                 shift
                 ;;
