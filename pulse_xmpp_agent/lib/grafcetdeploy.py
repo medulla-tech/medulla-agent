276052f5fd86f56144461b73385a497655ee6f53
Adrien T. <a.thaissen@gmail.com>
2024-09-20 11:32:20 +0200

    (AGENT) Fix conversion to UTF-8 in interaction question user
    (cherry picked from commit e2b9c732f2fc174f7ef7f0b769e9cb081065ba10)

--- pulse_xmpp_agent/lib/grafcetdeploy.py
+++ pulse_xmpp_agent/lib/grafcetdeploy.py
@@ -2404,6 +2404,12 @@ class grafcet:
                     % (self.data["name"], self.workingstep["step"])
                 )
             elif sys.platform.startswith("win"):
+                if isinstance(message, bytes):
+                    message = message.decode("utf-8")
+
+                if isinstance(titlemessage, bytes):
+                    titlemessage = titlemessage.decode("utf-8")
+
                 command = (
                     """C:\\progra~1\\Medulla\\bin\\paexec.exe -accepteula -s -i 1 """
                     """C:\\progra~1\\Python3\\pythonw C:\\progra~1\\Medulla\\bin\\pulse2_update_notification.py -M "%s" -B"%s" -t%s -Y "%s" -N "%s" -S%s -s%s -c"""
