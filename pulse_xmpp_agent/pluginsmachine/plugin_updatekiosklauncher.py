dbe0f1291a34362ca3af4d803f6158358fe824ff
SPO <stephane.pointu@siveo.net>
2024-11-05 15:38:16 +0100

    (kiosk) Update plugin for doing nothing to avoid errors on machines which already have the previous version of the plugin
    This plugin will be deleted at a later stage

---
+++ pulse_xmpp_agent/pluginsmachine/plugin_updatekiosklauncher.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2020-2023 Siveo <support@siveo.net>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from lib import utils
+
+plugin = {"VERSION": "2.0", "NAME": "updatekiosklauncher", "TYPE": "machine"}  # fmt: skip
+
+
+@utils.set_logging_level
+def action(xmppobject, action, sessionid, data, message, dataerreur):
+    pass
