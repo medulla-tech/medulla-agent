28d8be48aa559305ffc81aa2f3f48d0d52705713
Nicolas Lécureuil <neoclust@mageia.org>
2025-03-21 14:33:23 +0100

    New version 3.2.1
    (cherry picked from commit 4d786e8428061166fb3e519376af7dae92de0c7e)
    (cherry picked from commit ff9e6298f054512e73267cf3456345cf49e95eda)

--- packaging/python/agent_setup.py
+++ packaging/python/agent_setup.py
@@ -15,12 +15,12 @@ for scheme in list(INSTALL_SCHEMES.values()):
 
 setup(
     name="pulse_xmpp_agent",
-    version="3.2.0",
+    version="3.2.1",
     author="SIVEO",
-    author_email="support@siveo.net",
-    description="XMPP Agent for pulse",
+    author_email="contact@medulla-tech.io",
+    description="XMPP Agent for Medulla",
     long_description=long_description,
-    url="https://www.siveo.net/",
+    url="https://medulla-tech.io/",
     include_package_data=True,
     packages=[
         "pulse_xmpp_agent",
