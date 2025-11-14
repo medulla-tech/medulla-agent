bab661042937a69358ff942480af5454f827dfad
xdri3n <a.thaissen@gmail.com>
2025-11-14 11:12:29 +0100

    fix(asynchroremoteQA, resultasynchroremoteQA): We keep the newlines and we send and store the result encoded in base64

--- pulse_xmpp_agent/plugins_common/plugin_asynchroremoteQA.py
+++ pulse_xmpp_agent/plugins_common/plugin_asynchroremoteQA.py
@@ -1,18 +1,18 @@
-# -*- coding: utf-8 -*-
-# SPDX-FileCopyrightText: 2016-2023 Siveo <support@siveo.net>
+# -*- coding:Utf-8; -*
+# SPDX-FileCopyrightText: 2016-2023 Siveo, http://www.siveo.net
+# SPDX-FileCopyrightText: 2024-2025 Medulla, http://www.medulla-tech.io
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-
 from lib import utils
-import json
 import traceback
-import sys
 import logging
+import base64
+import json
+import sys
 
 logger = logging.getLogger()
 
-plugin = {"VERSION": "2.1", "NAME": "asynchroremoteQA", "TYPE": "all"}  # fmt: skip
-
+plugin = {"VERSION": "2.2", "NAME": "asynchroremoteQA", "TYPE": "all"}  # fmt: skip
 
 @utils.set_logging_level
 def action(objectxmpp, action, sessionid, data, message, dataerreur):
@@ -30,13 +30,26 @@ def action(objectxmpp, action, sessionid, data, message, dataerreur):
         resultcmd = utils.shellcommandtimeout(
             utils.encode_strconsole(data["data"]["customcmd"]), 15
         ).run()
+
         resultcmd["result"] = [utils.decode_strconsole(x) for x in resultcmd["result"]]
+
+        # Join lines with newlines
+        result_as_string = '\n'.join(resultcmd["result"])
+
+        # Encode in base64 to preserve all special characters
+        result_encoded = base64.b64encode(result_as_string.encode('utf-8')).decode('ascii')
+
+        # Replace the list with the base64 string
+        resultcmd["result"] = result_encoded
+
         result["data"]["result"] = resultcmd
         result["data"]["data"] = data
         result["ret"] = resultcmd["code"]
+
         objectxmpp.send_message(
             mto=message["from"], mbody=json.dumps(result), mtype="chat"
         )
+
     except Exception as e:
         logger.error(str(e))
         logger.error("\n%s" % (traceback.format_exc()))
--- pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_resultasynchroremoteQA.py
+++ pulse_xmpp_master_substitute/pluginsmastersubstitute/plugin_resultasynchroremoteQA.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-# -*- coding: utf-8; -*-
+# -*- coding:Utf-8; -*
+# SPDX-FileCopyrightText: 2024-2025 Medulla, http://www.medulla-tech.io
+# SPDX-License-Identifier: GPL-3.0-or-later
 
-import logging
-import traceback
-import sys
-import json
 from lib.plugins.xmpp import XmppMasterDatabase
+import traceback
+import logging
+import json
+import sys
 
 logger = logging.getLogger()
 
-plugin = {"VERSION": "1.0", "NAME": "resultasynchroremoteQA", "TYPE": "mastersub"}
-
+plugin = {"VERSION": "1.1", "NAME": "resultasynchroremoteQA", "TYPE": "mastersub"}
 
 def action(xmppobject, action, sessionid, data, message, ret, dataobj):
     logger.debug("=====================================================")
     logger.debug(plugin)
     logger.debug("=====================================================")
     try:
+        result_data = data["result"]["result"]
+
+        # If it is a list, join the elements (backwards compatibility)
+        if isinstance(result_data, list):
+            result_string = "".join(result_data)
+        else:
+            result_string = result_data
+
         XmppMasterDatabase().setCommand_action(
             data["data"]["data"]["uuid_inventorymachine"],
             data["data"]["data"]["cmdid"],
             sessionid,
-            "".join(data["result"]["result"]),
+            result_string,
             typemessage="result",
         )
 
     except Exception as e:
         logger.error("Error loading plugin: %s" % str(e))
         traceback.print_exc(file=sys.stdout)
-        pass
