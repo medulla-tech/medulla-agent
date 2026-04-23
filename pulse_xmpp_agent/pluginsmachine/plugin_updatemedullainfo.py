f6bf42b5a9fc633d07fe029c0e6f732ce4bb6d06
SPO <spointu@medulla-tech.io>
2026-04-23 09:12:48 +0200

    Revert "(update) Fix plugin updatemedullainfo version"
    This reverts commit afc5cd81859f27a605b5db516c00039b730cdee1.

--- pulse_xmpp_agent/pluginsmachine/plugin_updatemedullainfo.py
+++ pulse_xmpp_agent/pluginsmachine/plugin_updatemedullainfo.py
@@ -30,7 +30,7 @@ if sys.platform.startswith("win"):
 logger = logging.getLogger()
 
 
-plugin = {"VERSION": "1.17", "NAME": "updatemedullainfo", "TYPE": "machine"}  # fmt: skip
+plugin = {"VERSION": "1.161", "NAME": "updatemedullainfo", "TYPE": "machine"}  # fmt: skip
 LATEST_WIN10 = "22H2"
 LATEST_WIN11 = "25H2"
 LATEST_SERVER_ISO = "2025_24H2"
