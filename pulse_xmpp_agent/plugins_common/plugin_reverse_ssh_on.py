e67c41736851fb0c5d3f0a96fe563f11e569d461
SPO <spointu@medulla-tech.io>
2026-09-16 17:38:42 +0200

    (reversessh) Use the same ssh command on Windows, Linux and MacOS clients

--- pulse_xmpp_agent/plugins_common/plugin_reverse_ssh_on.py
+++ pulse_xmpp_agent/plugins_common/plugin_reverse_ssh_on.py
@@ -27,7 +27,7 @@ if sys.platform.startswith("win"):
     import win32api
 
 logger = logging.getLogger()
-plugin = {"VERSION": "4.4", "NAME": "reverse_ssh_on", "TYPE": "all"}  # fmt: skip
+plugin = {"VERSION": "4.5", "NAME": "reverse_ssh_on", "TYPE": "all"}  # fmt: skip
 
 
 def checkresult(result):
@@ -222,7 +222,7 @@ def action(objectxmpp, action, sessionid, data, message, dataerreur):
                     os.path.expanduser("~pulseuser"), ".ssh", "id_rsa"
                 )
                 dd = """#!/bin/bash
-                /usr/bin/ssh -t -t -%s %s:localhost:%s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "%s" -l reversessh %s -p %s&
+                /usr/bin/ssh -N -T -%s %s:localhost:%s -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i "%s" -l reversessh %s -p %s&
                 """ % (
                     reversetype,
                     data["port"],
